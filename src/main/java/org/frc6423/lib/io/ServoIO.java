864e6be431d55e307ac4e673c067d59ca4d9d57b
dabeycorn <Dasun20202020@gmail.com>
2026-01-09 12:08:50 -0500

    IO: migration ServoIO and its implementations from y2025
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

---
+++ src/main/java/org/frc6423/lib/io/ServoIO.java
@@ -0,0 +1,411 @@
+// Copyright (c) 2026 FRC 6423 - Ward Melville Iron Patriots
+// https://github.com/wmironpatriots
+// 
+// Open Source Software; you can modify and/or share it under the terms of
+// MIT license file in the root directory of this project
+
+package org.frc6423.lib.io;
+
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Celsius;
+import static edu.wpi.first.units.Units.Volts;
+
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.epilogue.Logged.Importance;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularAcceleration;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.Alert.AlertType;
+import edu.wpi.first.wpilibj.DriverStation;
+import java.util.function.UnaryOperator;
+
+/** A I/O interface for controlling a servo */
+public abstract class ServoIO {
+  public final String name;
+
+  private final Temperature maxTemp;
+  private Alert shutdownAlert =
+      new Alert(getName() + " is overheating! SHUTTING DOWN to prevent damage!", AlertType.kError);
+
+  private Setpoint currentSetpoint = Setpoint.stop();
+  private boolean enabled = false;
+
+  /**
+   * Create new {@link ServoIO}
+   *
+   * @param name friendly "nickname" for servo
+   */
+  protected ServoIO(String name) {
+    this(name, Celsius.of(75));
+  }
+
+  /**
+   * Create new {@link ServoIO}
+   *
+   * @param name friendly "nickname" for servo
+   * @param shutdownTemperature {@link Temperature} representing the maximum temperature a servo can
+   *     be at before shutting down
+   */
+  protected ServoIO(String name, Temperature shutdownTemperature) {
+    this.name = name;
+    this.maxTemp = shutdownTemperature;
+  }
+
+  /** Periodic Logic Should be called every robot loop */
+  public void periodic() {
+    // Disables and alerts drive team if servo is overheating
+    if (getTemperature().gt(maxTemp)) {
+      shutdownAlert.set(true);
+      disable();
+    } else if (shutdownAlert.get()) {
+      shutdownAlert.set(false);
+      enable();
+    }
+  }
+
+  /**
+   * @return friendly nickname for servo
+   */
+  @Logged(name = "Servo Name", importance = Importance.INFO)
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * @return true if servo control is enabled
+   */
+  @Logged(name = "Is Enabled", importance = Importance.INFO)
+  public boolean isEnabled() {
+    return enabled;
+  }
+
+  /**
+   * @return current {@link Setpoint}
+   */
+  public Setpoint getCurrentSetpoint() {
+    return currentSetpoint;
+  }
+
+  /**
+   * @return {@link Voltage} representing the applied voltage of servo
+   */
+  @Logged(name = "Applied Voltage", importance = Importance.INFO)
+  public abstract Voltage getAppliedVoltage();
+
+  /**
+   * @return {@link Current} representing the input supply current of servo
+   */
+  @Logged(name = "Supply Current", importance = Importance.INFO)
+  public abstract Current getSupplyCurrent();
+
+  /**
+   * @return {@link Current} representing the output stator current of servo
+   */
+  @Logged(name = "Stator Current", importance = Importance.INFO)
+  public abstract Current getStatorCurrent();
+
+  /**
+   * @return {@link Current} representing the torque current output of servo
+   */
+  @Logged(name = "Torque Current", importance = Importance.INFO)
+  public abstract Current getTorqueCurrent();
+
+  /**
+   * @return {@link Angle} representing the angular position of servo
+   */
+  @Logged(name = "Angle", importance = Importance.INFO)
+  public abstract Angle getAngle();
+
+  /**
+   * @return {@link AngularVelocity} representing the angular velocity of servo
+   */
+  @Logged(name = "Angular Velocity", importance = Importance.INFO)
+  public abstract AngularVelocity getAngularVelocity();
+
+  /**
+   * @return {@link AngularAcceleration} representing the angular acceleration of servo
+   */
+  @Logged(name = "Angular Acceleration", importance = Importance.INFO)
+  public abstract AngularAcceleration getAngularAcceleration();
+
+  /**
+   * @return {@link Temperature} representing the temperature of servo
+   */
+  @Logged(name = "Temperature", importance = Importance.INFO)
+  public abstract Temperature getTemperature();
+
+  /** Enable servo control Servo will only enable if DriverStation is enabled */
+  public void enable() {
+    if (!DriverStation.isEnabled()) {
+      enabled = true;
+    }
+
+    // Reapply setpoint
+    applySetpoint(currentSetpoint);
+  }
+
+  /** Disable servo control */
+  public void disable() {
+    enabled = false;
+
+    // Set idle mode to brake
+    enableBrake();
+
+    // stop all movement
+    idle();
+  }
+
+  /**
+   * Run {@link Setpoint}
+   *
+   * @param setpoint {@link Setpoint} representing desired setpoint
+   */
+  public void applySetpoint(Setpoint setpoint) {
+    this.currentSetpoint = setpoint;
+
+    if (enabled) {
+      setpoint.applySetpoint(this);
+    }
+  }
+
+  /** Enable brake mode */
+  public abstract void enableBrake();
+
+  /** Disable brake mode */
+  public abstract void disableBrake();
+
+  /** Stop all servo control */
+  protected abstract void idle();
+
+  /**
+   * Set voltage setpoint
+   *
+   * @param volts {@link Voltage} representing desired voltage
+   */
+  protected abstract void setVoltageSetpoint(Voltage voltage);
+
+  /**
+   * Set Torque Current Setpoint Only works for FOC supported servos; Will not run on non-FOC
+   * supported servos
+   *
+   * @param current {@link Current} representing desired torque current
+   */
+  protected abstract void setTorqueCurrentSetpoint(Current current);
+
+  /**
+   * Set dutycycle setpoint
+   *
+   * @param dutycycle desired dutycycle output
+   */
+  protected abstract void setDutycycleSetpoint(double dutycycle);
+
+  /**
+   * Set position setpoint
+   *
+   * @param position {@link Angle} representing desired angular position
+   * @param slot control gains slot to use
+   */
+  protected abstract void setPositionSetpoint(Angle position, int slot);
+
+  /**
+   * Set velocity setpoint
+   *
+   * @param velocity {@link AngularVelocity} representing desired angular velocity
+   * @param acceleration {@link AngularAcceleration} representing desired angular acceleration
+   * @param slot control gains slot to use
+   */
+  protected abstract void setVelocitySetpoint(
+      AngularVelocity velocity, AngularAcceleration acceleration, int slot);
+
+  /**
+   * Set position motion profiled setpoint
+   *
+   * @param position {@link Angle} representing desired angular position
+   * @param slot control gains slot to use
+   */
+  protected abstract void setPositionProfiledSetpoint(Angle position, int slot);
+
+  /**
+   * Set velocity motion profiled setpoint
+   *
+   * @param velocity {@link AngularVelocity} representing desired angular velocity
+   * @param acceleration {@link AngularAcceleration} representing desired angular acceleration
+   * @param slot control gains slot to use
+   */
+  protected abstract void setVelocityProfiledSetpoint(
+      AngularVelocity velocity, AngularAcceleration acceleration, int slot);
+
+  public static class Setpoint implements Sendable {
+    /** Represents a method of Motor Control */
+    public static enum ControlType {
+      /** Idle */
+      IDLE,
+      /** Open loop voltage control */
+      VOLTAGE,
+      /** Open loop torque current control (FOC only) */
+      TORQUE_CURRENT,
+      /** Open loop dutycycle control */
+      DUTYCYCLE,
+      /** Closed loop position control */
+      POSITION,
+      /** Closed loop velocity control */
+      VELOCITY,
+      /** Motion Profiled closed loop position control */
+      POSITION_PROFILED,
+      /** Motion Profiled closed loop velocity control */
+      VELOCITY_PROFILED;
+
+      /**
+       * @return true if {@link ControlType} selected is a form of open loop control
+       */
+      public boolean isOpenLoopControl() {
+        if (this == VOLTAGE || this == TORQUE_CURRENT || this == DUTYCYCLE) {
+          return true;
+        }
+
+        return false;
+      }
+
+      /**
+       * @return true if {@link ControlType} selected is a form of closed loop control
+       */
+      public boolean isClosedLoopControl() {
+        if (!isOpenLoopControl() && this == IDLE) {
+          return true;
+        }
+
+        return false;
+      }
+
+      /**
+       * @return true if {@link ControlType} selected is a form of closed loop position control
+       */
+      public boolean isPositionControl() {
+        if (this == POSITION || this == POSITION_PROFILED) {
+          return true;
+        }
+
+        return false;
+      }
+
+      /**
+       * @return true if {@link ControlType} selected is a form of closed loop velocity control
+       */
+      public boolean isVelocityControl() {
+        if (this == POSITION || this == POSITION_PROFILED) {
+          return true;
+        }
+
+        return false;
+      }
+    }
+
+    private final double value;
+    private final ControlType type;
+    private final UnaryOperator<ServoIO> applier;
+
+    private Setpoint(double value, ControlType type, UnaryOperator<ServoIO> applier) {
+      this.value = value;
+      this.type = type;
+      this.applier = applier;
+    }
+
+    /**
+     * @return get {@link Setpoint} value in BaseUnits
+     */
+    public double getValue() {
+      return value;
+    }
+
+    /**
+     * @return get {@link Setpoint} {@link ControlType}
+     */
+    public ControlType getControlType() {
+      return type;
+    }
+
+    @Override
+    public void initSendable(SendableBuilder builder) {
+      builder.addStringProperty("Type", () -> getControlType().toString(), null);
+      builder.addDoubleProperty("Value", this::getValue, null);
+    }
+
+    /**
+     * Create new idle {@link Setpoint}
+     *
+     * @return {@link Setpoint}
+     */
+    public static Setpoint stop() {
+      UnaryOperator<ServoIO> applier =
+          (ServoIO io) -> {
+            io.idle();
+            return io;
+          };
+
+      return new Setpoint(0, ControlType.IDLE, applier);
+    }
+
+    /**
+     * Create new Voltage {@link Setpoint}
+     *
+     * @param voltage {@link Voltage} representing desired voltage
+     * @return {@link Setpoint}
+     */
+    public static Setpoint createVoltageSetpoint(Voltage voltage) {
+      UnaryOperator<ServoIO> applier =
+          (ServoIO io) -> {
+            io.setVoltageSetpoint(voltage);
+            return io;
+          };
+
+      return new Setpoint(voltage.in(Volts), ControlType.VOLTAGE, applier);
+    }
+
+    /**
+     * Create new Torque Current {@link Setpoint}
+     *
+     * @param torqueCurrent {@link Current} representing desired torque current
+     * @return {@link Setpoint}
+     */
+    public static Setpoint createTorqueCurrentSetpoint(Current torqueCurrent) {
+      UnaryOperator<ServoIO> applier =
+          (ServoIO io) -> {
+            io.setTorqueCurrentSetpoint(torqueCurrent);
+            return io;
+          };
+
+      return new Setpoint(torqueCurrent.in(Amps), ControlType.VOLTAGE, applier);
+    }
+
+    /**
+     * Create new Dutycycle {@link Setpoint}
+     *
+     * @param dutycycle desired dutycycle
+     * @return {@link Setpoint}
+     */
+    public static Setpoint createDutycycleSetpoint(double dutycycle) {
+      UnaryOperator<ServoIO> applier =
+          (ServoIO io) -> {
+            io.setDutycycleSetpoint(dutycycle);
+            return io;
+          };
+
+      return new Setpoint(dutycycle, ControlType.VOLTAGE, applier);
+    }
+
+    /**
+     * Apply setpoint request to {@link ServoIO}
+     *
+     * @param io {@link ServoIO} to run {@link Setpoint}
+     */
+    void applySetpoint(ServoIO io) {
+      applier.apply(io);
+    }
+  }
+}
---
+++ src/main/java/org/frc6423/lib/io/ServoIOTalonFx.java
@@ -0,0 +1,282 @@
+// Copyright (c) 2026 FRC 6423 - Ward Melville Iron Patriots
+// https://github.com/wmironpatriots
+// 
+// Open Source Software; you can modify and/or share it under the terms of
+// MIT license file in the root directory of this project
+
+package org.frc6423.lib.io;
+
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Celsius;
+import static edu.wpi.first.units.Units.RadiansPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Revolutions;
+import static edu.wpi.first.units.Units.RevolutionsPerSecond;
+import static edu.wpi.first.units.Units.Volts;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.MotionMagicVelocityTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.PositionTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.TorqueCurrentFOC;
+import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularAcceleration;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.function.UnaryOperator;
+
+/**
+ * Implementation of {@link ServoIO} for TalonFX Servos {@see
+ * https://v6.docs.ctr-electronics.com/en/latest/docs/hardware-reference/talonfx/index.html} for
+ * more
+ */
+public class ServoIOTalonFx extends ServoIO {
+  private final TalonFX servo;
+  private final TalonFXConfiguration config;
+
+  private final BaseStatusSignal voltageSignal,
+      inCurrentSignal,
+      outCurrentSignal,
+      torqueCurrentSignal,
+      angleSignal,
+      velocitySignal,
+      accelerationSignal,
+      temperatureSignal;
+
+  private final VoltageOut voltageRequest = new VoltageOut(0.0).withEnableFOC(false);
+  private final TorqueCurrentFOC torqueRequest = new TorqueCurrentFOC(0.0);
+  private final DutyCycleOut dutycycleRequest = new DutyCycleOut(0.0).withEnableFOC(false);
+  private final PositionTorqueCurrentFOC focPositionRequest = new PositionTorqueCurrentFOC(0.0);
+  private final PositionVoltage positionRequest = new PositionVoltage(0.0).withEnableFOC(false);
+  private final VelocityTorqueCurrentFOC focVelocityRequest = new VelocityTorqueCurrentFOC(0.0);
+  private final VelocityVoltage velocityRequest = new VelocityVoltage(0.0).withEnableFOC(false);
+  private final MotionMagicTorqueCurrentFOC focProfiledPositionRequest =
+      new MotionMagicTorqueCurrentFOC(0.0);
+  private final MotionMagicVoltage profiledPositionRequest =
+      new MotionMagicVoltage(0.0).withEnableFOC(false);
+  private final MotionMagicVelocityTorqueCurrentFOC focProfiledVelocityRequest =
+      new MotionMagicVelocityTorqueCurrentFOC(0.0);
+  private final MotionMagicVelocityVoltage profiledVelocityRequest =
+      new MotionMagicVelocityVoltage(0.0).withEnableFOC(false);
+
+  private boolean focEnabled = true;
+
+  private final BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>();
+  private final ThreadPoolExecutor threadPoolExecutor =
+      new ThreadPoolExecutor(1, 1, 5, java.util.concurrent.TimeUnit.MILLISECONDS, queue);
+
+  /**
+   * Create new {@link ServoIOTalonFx}
+   *
+   * @param name friendly "nickname" for servo
+   * @param canDeviceId integer representing the CAN identification
+   * @param canBusId {@link CANBus} representing the CAN bus device is on
+   * @param config {@link TalonFXConfiguration} representing servo config
+   * @param temperature {@link Temperature} representing the maximum temperature a servo can be at
+   *     before shutting down
+   */
+  public ServoIOTalonFx(
+      String name,
+      int canDeviceId,
+      CANBus canBus,
+      TalonFXConfiguration config,
+      Temperature temperature) {
+    super(name, temperature);
+
+    this.servo = new TalonFX(canDeviceId, canBus);
+    this.config = config;
+
+    this.voltageSignal = servo.getMotorVoltage(true);
+    this.inCurrentSignal = servo.getSupplyCurrent(true);
+    this.outCurrentSignal = servo.getStatorCurrent(true);
+    this.torqueCurrentSignal = servo.getTorqueCurrent(true);
+
+    this.angleSignal = servo.getPosition(true);
+    this.velocitySignal = servo.getVelocity(true);
+    this.accelerationSignal = servo.getAcceleration(true);
+    this.temperatureSignal = servo.getDeviceTemp(true);
+  }
+
+  /**
+   * Apply new {@link TalonFXConfiguration}
+   *
+   * @param configApplier {@link UnaryOperator} that modifies a {@link TalonFXConfiguration}
+   */
+  public void applyConfig(UnaryOperator<TalonFXConfiguration> configApplier) {
+    threadPoolExecutor.submit(
+        () -> {
+          for (int i = 0; i < 5; i++) {
+            StatusCode result = servo.getConfigurator().apply(configApplier.apply(config));
+            if (result.isOK()) {
+              break;
+            }
+          }
+        });
+  }
+
+  @Override
+  public Voltage getAppliedVoltage() {
+    return Volts.of(voltageSignal.getValueAsDouble());
+  }
+
+  @Override
+  public Current getSupplyCurrent() {
+    return Amps.of(inCurrentSignal.getValueAsDouble());
+  }
+
+  @Override
+  public Current getStatorCurrent() {
+    return Amps.of(outCurrentSignal.getValueAsDouble());
+  }
+
+  @Override
+  public Current getTorqueCurrent() {
+    return Amps.of(torqueCurrentSignal.getValueAsDouble());
+  }
+
+  @Override
+  public Angle getAngle() {
+    return Revolutions.of(angleSignal.getValueAsDouble());
+  }
+
+  @Override
+  public AngularVelocity getAngularVelocity() {
+    return RevolutionsPerSecond.of(velocitySignal.getValueAsDouble());
+  }
+
+  @Override
+  public AngularAcceleration getAngularAcceleration() {
+    return RadiansPerSecondPerSecond.of(2 * Math.PI * accelerationSignal.getValueAsDouble());
+  }
+
+  @Override
+  public Temperature getTemperature() {
+    return Celsius.of(temperatureSignal.getValueAsDouble());
+  }
+
+  @Override
+  public void enableBrake() {
+    UnaryOperator<TalonFXConfiguration> configApplier =
+        (TalonFXConfiguration config) -> {
+          config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+
+          return config;
+        };
+
+    applyConfig(configApplier);
+  }
+
+  @Override
+  public void disableBrake() {
+    UnaryOperator<TalonFXConfiguration> configApplier =
+        (TalonFXConfiguration config) -> {
+          config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+
+          return config;
+        };
+
+    applyConfig(configApplier);
+  }
+
+  /**
+   * Enable Field-Oriented Control (FOC) {@see
+   * https://v6.docs.ctr-electronics.com/en/latest/docs/api-reference/device-specific/talonfx/talonfx-control-intro.html#field-oriented-control}
+   * <strong>WARNING</strong>: FOC control calculates a Torque Current system input (in Amps). This
+   * means gains that work with voltage based control will never work with FOC based control
+   */
+  public void enableFoc() {
+    focEnabled = true;
+  }
+
+  /**
+   * Disable Field-Oriented Control (FOC) {@see
+   * https://v6.docs.ctr-electronics.com/en/latest/docs/api-reference/device-specific/talonfx/talonfx-control-intro.html#field-oriented-control}
+   * <strong>WARNING</strong>: Disabling FOC will use voltage based control for all controll methods
+   * and will produce a Voltage system input (in Volts). This means gains that work with FOC based
+   * control will never work with voltage based control
+   */
+  public void disableFoc() {
+    focEnabled = false;
+  }
+
+  @Override
+  protected void idle() {
+    servo.stopMotor();
+  }
+
+  @Override
+  protected void setVoltageSetpoint(Voltage voltage) {
+    servo.setControl(voltageRequest.withOutput(voltage).withEnableFOC(focEnabled));
+  }
+
+  @Override
+  protected void setTorqueCurrentSetpoint(Current current) {
+    servo.setControl(torqueRequest.withOutput(current));
+  }
+
+  @Override
+  protected void setDutycycleSetpoint(double dutycycle) {
+    servo.setControl(dutycycleRequest.withOutput(dutycycle).withEnableFOC(focEnabled));
+  }
+
+  @Override
+  protected void setPositionSetpoint(Angle position, int slot) {
+    if (focEnabled) {
+      focPositionRequest.withPosition(position).withSlot(slot);
+      return;
+    }
+
+    positionRequest.withPosition(position).withSlot(slot);
+  }
+
+  @Override
+  protected void setVelocitySetpoint(
+      AngularVelocity velocity, AngularAcceleration acceleration, int slot) {
+    if (focEnabled) {
+      focVelocityRequest.withVelocity(velocity);
+      return;
+    }
+
+    velocityRequest.withVelocity(velocity).withAcceleration(acceleration).withSlot(slot);
+  }
+
+  @Override
+  protected void setPositionProfiledSetpoint(Angle position, int slot) {
+    if (focEnabled) {
+      focProfiledPositionRequest.withPosition(position).withSlot(slot);
+      return;
+    }
+
+    profiledPositionRequest.withPosition(position).withSlot(slot);
+  }
+
+  @Override
+  protected void setVelocityProfiledSetpoint(
+      AngularVelocity velocity, AngularAcceleration acceleration, int slot) {
+    if (focEnabled) {
+      focProfiledVelocityRequest
+          .withVelocity(velocity)
+          .withAcceleration(acceleration)
+          .withSlot(slot);
+      return;
+    }
+
+    profiledVelocityRequest.withVelocity(velocity).withAcceleration(acceleration).withSlot(slot);
+  }
+}
