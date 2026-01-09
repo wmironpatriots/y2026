b507526512ec9e19ba1f43e459ce186ef7adb5a6
Dasun A. <Dasun20202020@hotmail.com>
2026-01-09 11:48:50 -0500

    (0.0.0d) Setup 2026 WPIlib project (#6)
    * WPIlib: generate 2026 Command-Based skeleton template project
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    
    * gradle: add spotless, gversion, profiling mode, and installGitHooks cmmd
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    
    * git: add pre-commit hooks
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    
    * gradle: change Main class path from frc.robot.Main to org.frc6423.robot.Main
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    
    * lib: migrate relevant classes from previous year's lib
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    
    * lib.CommandRobot: remove deprecated methods
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    
    * robot.Robot: add ds & epilogue configs
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    
    * gitignore: add gversion generate files
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    
    * github: add build, fix-format, check-tests workflow
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    
    * base readme (#9)
    
    * README: create base
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    
    * Create README.md base
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    
    * rendering fixes
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    
    ---------
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    Signed-off-by: Dasun A. <Dasun20202020@hotmail.com>
    
    * gradle: change version to 0.0.0d
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    
    ---------
    
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>
    Signed-off-by: Dasun A. <Dasun20202020@hotmail.com>

---
+++ src/main/java/org/frc6423/lib/driver/CommandRobot.java
@@ -0,0 +1,112 @@
+// Copyright (c) 2026 FRC 6423 - Ward Melville Iron Patriots
+// https://github.com/wmironpatriots
+// 
+// Open Source Software; you can modify and/or share it under the terms of
+// MIT license file in the root directory of this project
+
+package org.frc6423.lib.driver;
+
+import static edu.wpi.first.units.Units.Seconds;
+
+import edu.wpi.first.units.measure.Time;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
+import org.frc6423.lib.util.Tracer;
+
+/**
+ * {@link CommandRobot} is an extension of {@link TimedRobot}
+ *
+ * <p>The {@link CommandRobot} class is intendended to subclassed by a user creating a Robot Program
+ *
+ * <p>The {@link CommandScheduler} is automatically called and traced by {@link Tracer} every period
+ *
+ * <p>Abstract method getAutonomousCommand() is automatically scheduled on autonomous initialization
+ *
+ * <p>Garbage collector automatically called every 5 seconds
+ *
+ * @see https://github.com/wpilibsuite/allwpilib/pull/5939
+ * @see {@link TimedRobot}
+ */
+public abstract class CommandRobot extends TimedRobot {
+  private final CommandScheduler scheduler = CommandScheduler.getInstance();
+
+  private Command autonCommand = Commands.none();
+
+  private final Timer gcTimer = new Timer();
+
+  /** Create new {@link CommandRobot} with looptime of 20 milliseconds */
+  public CommandRobot() {
+    this(Seconds.of(kDefaultPeriod));
+  }
+
+  /**
+   * Create new {@link CommandRobot} with specified looptime
+   *
+   * @param period looptime in period
+   */
+  public CommandRobot(Time period) {
+    super(period.in(Seconds));
+
+    // Start garbage collection timer
+    gcTimer.start();
+  }
+
+  @Override
+  public void robotPeriodic() {
+    // Run and trace CommandScheduler
+    Tracer.traceFunc("CommandScheduler", scheduler::run);
+
+    // Run Garbage Collector every 5 seconds
+    if (gcTimer.hasElapsed(5)) {
+      System.gc();
+    }
+  }
+
+  @Override
+  public void disabledInit() {
+    scheduler.cancelAll();
+    System.gc();
+  }
+
+  @Override
+  public void disabledExit() {
+    scheduler.cancelAll();
+    System.gc();
+  }
+
+  @Override
+  public void autonomousInit() {
+    autonCommand = getAutonCommand();
+
+    if (autonCommand != null) {
+      scheduler.schedule(autonCommand);
+    }
+  }
+
+  @Override
+  public void autonomousExit() {
+    scheduler.cancelAll();
+    System.gc();
+  }
+
+  @Override
+  public void teleopInit() {
+    if (autonCommand != null) {
+      autonCommand.cancel();
+    }
+  }
+
+  @Override
+  public void teleopExit() {
+    scheduler.cancelAll();
+    System.gc();
+  }
+
+  /**
+   * @return {@link Command} to schedule on autonomous oppmode initialization
+   */
+  protected abstract Command getAutonCommand();
+}
---
+++ src/main/java/org/frc6423/robot/Robot.java
@@ -0,0 +1,83 @@
+// Copyright (c) 2026 FRC 6423 - Ward Melville Iron Patriots
+// https://github.com/wmironpatriots
+// 
+// Open Source Software; you can modify and/or share it under the terms of
+// MIT license file in the root directory of this project
+
+package org.frc6423.robot;
+
+import edu.wpi.first.epilogue.Epilogue;
+import edu.wpi.first.epilogue.EpilogueConfiguration;
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.epilogue.Logged.Importance;
+import edu.wpi.first.epilogue.logging.LazyBackend;
+import edu.wpi.first.epilogue.logging.NTEpilogueBackend;
+import edu.wpi.first.epilogue.logging.errors.ErrorHandler;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import org.frc6423.lib.driver.CommandRobot;
+
+@Logged
+public class Robot extends CommandRobot {
+  public Robot() {
+    // Shut up DS
+    DriverStation.silenceJoystickConnectionWarning(true);
+
+    // Configure Epilogue
+    Epilogue.configure(
+        (EpilogueConfiguration config) -> {
+          // Set log data path
+          config.root = "Telemetry";
+
+          // Lazylog to NT
+          config.backend =
+              new LazyBackend(new NTEpilogueBackend(NetworkTableInstance.getDefault()));
+
+          // Start logging
+          if (Robot.isReal()) {
+            DataLogManager.start();
+          }
+
+          // Set error handling
+          if (Robot.isSimulation()) {
+            config.errorHandler = ErrorHandler.crashOnError();
+          } else {
+            config.errorHandler = ErrorHandler.printErrorMessages();
+          }
+
+          // Set lowest importance level to be logged
+          config.minimumImportance = Importance.DEBUG;
+        });
+
+    // Bind Epilogue to robot periodic
+    Epilogue.bind(this);
+
+    // Log metadata
+    final String metadataPath = "/BuildData/";
+    var config = Epilogue.getConfig();
+    config.backend.log(metadataPath + "RuntimeType", getRuntimeType().toString());
+    config.backend.log(metadataPath + "ProjectName", BuildConstants.MAVEN_NAME);
+    config.backend.log(metadataPath + "Version", BuildConstants.VERSION);
+    config.backend.log(metadataPath + "GitDirty", BuildConstants.DIRTY);
+    config.backend.log(metadataPath + "GitRev", BuildConstants.GIT_REVISION);
+    config.backend.log(metadataPath + "GitSHA", BuildConstants.GIT_SHA);
+    config.backend.log(metadataPath + "GitDate", BuildConstants.GIT_DATE);
+    config.backend.log(metadataPath + "GitBranch", BuildConstants.GIT_BRANCH);
+    config.backend.log(metadataPath + "BuildDate", BuildConstants.BUILD_DATE);
+    config.backend.log(metadataPath + "BuildUnixTime", BuildConstants.BUILD_UNIX_TIME);
+  }
+
+  /** Define Driver & Operator controller bindings */
+  public void configureBindings() {}
+
+  /** Define behavior during different oppmodes */
+  public void configureGameBehavior() {}
+
+  @Override
+  protected Command getAutonCommand() {
+    return Commands.none();
+  }
+}
