5db49266413017335e480063acd06a5e357977ac
dabeycorn <Dasun20202020@gmail.com>
2026-01-09 09:48:16 -0500

    gradle: add spotless, gversion, profiling mode, and installGitHooks cmmd
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,6 +1,8 @@
-// Copyright (c) FIRST and other WPILib contributors.
+// Copyright (c) 2026 FRC 6423 - Ward Melville Iron Patriots
+// https://github.com/wmironpatriots
+// 
 // Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// MIT license file in the root directory of this project
 
 package frc.robot;
 
---
+++ src/main/java/org/frc6423/robot/BuildConstants.java
@@ -0,0 +1,23 @@
+// Copyright (c) 2026 FRC 6423 - Ward Melville Iron Patriots
+// https://github.com/wmironpatriots
+// 
+// Open Source Software; you can modify and/or share it under the terms of
+// MIT license file in the root directory of this project
+
+package org.frc6423.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "y2026";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "9114e7fc8b98f67826c77ee44a84874d548eb1e5";
+  public static final String GIT_DATE = "2026-01-09T01:53:12Z";
+  public static final String GIT_BRANCH = "chore/generate-project";
+  public static final String BUILD_DATE = "2026-01-09T10:47:15Z";
+  public static final long BUILD_UNIX_TIME = 1767970035768L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants() {}
+}
