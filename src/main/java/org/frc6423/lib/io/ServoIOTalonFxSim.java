55c105973f812f6d35a5bc97326fb8c0d3023ab4
dabeycorn <Dasun20202020@gmail.com>
2026-01-13 10:56:54 -0500

    ServoIOTalonFxSim: base implementation
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

--- src/main/java/org/frc6423/lib/io/ServoIOTalonFxSim.java
+++ src/main/java/org/frc6423/lib/io/ServoIOTalonFxSim.java
@@ -7,158 +7,120 @@
 package org.frc6423.lib.io;
 
 import static edu.wpi.first.units.Units.Celsius;
+import static edu.wpi.first.units.Units.Radians;
 
 import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.sim.ChassisReference;
-import com.ctre.phoenix6.sim.TalonFXSimState;
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.epilogue.Logged.Importance;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
-import edu.wpi.first.wpilibj.simulation.ElevatorSim;
-import edu.wpi.first.wpilibj.simulation.FlywheelSim;
-import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
-import java.util.Optional;
 
 /** Simulated extension of {@link ServoIOTalonFx} */
 public class ServoIOTalonFxSim extends ServoIOTalonFx {
-  private final ChassisReference positiveDirection;
+  private final DCMotorSim sim;
 
-  private final Optional<DCMotorSim> dcMotorSim;
-  private final Optional<FlywheelSim> flywheelSim;
-  private final Optional<SingleJointedArmSim> armSim;
-  private final Optional<ElevatorSim> elevatorSim;
+  private boolean invertVoltage = false;
+
+  private Angle simAngle = Radians.of(0.0);
+
+  private final Notifier simUpdater;
+  private double previousUpdateTimestamp = 0.0;
 
   /**
-   * Create new {@link ServoIOTalonFxSim} with a {@link DCMotorSim} backend
+   * Create new {@link ServoIOTalonFxSim}
    *
    * @param name friendly "nickname" for servo
    * @param canDeviceId integer representing the CAN identification
    * @param canBusId {@link CANBus} representing the CAN bus device is on
    * @param config {@link TalonFXConfiguration} representing servo config
-   * @param positiveDirection {@link ChassisReference} representing the positive angular direction
-   *     of the servo
-   * @param dcMotorSim {@link DCMotorSim} representing the physics model the simulation should use
-   *     as backend
+   * @param jKgMetersSquared Moment of Inertia in jKgM^2
    */
   public ServoIOTalonFxSim(
       String name,
       int canDeviceId,
       CANBus canBus,
       TalonFXConfiguration config,
-      ChassisReference positiveDirection,
-      DCMotorSim dcMotorSim) {
-    super(name, canDeviceId, canBus, config, Celsius.of(999));
+      double jKgMetersSquared) {
+    super(name, canDeviceId, canBus, config, Celsius.of(9999));
 
-    this.positiveDirection = positiveDirection;
+    sim =
+        new DCMotorSim(
+            LinearSystemId.createDCMotorSystem(
+                DCMotor.getKrakenX60Foc(1),
+                jKgMetersSquared,
+                1.0 / config.Feedback.SensorToMechanismRatio),
+            DCMotor.getKrakenX60Foc(1),
+            0.001,
+            0.001);
 
-    this.dcMotorSim = Optional.of(dcMotorSim);
-    this.flywheelSim = Optional.empty();
-    this.armSim = Optional.empty();
-    this.elevatorSim = Optional.empty();
+    servo.getSimState().Orientation =
+        config.MotorOutput.Inverted == InvertedValue.CounterClockwise_Positive
+            ? ChassisReference.CounterClockwise_Positive
+            : ChassisReference.Clockwise_Positive;
+
+    simUpdater = new Notifier(() -> updateSimulation());
+    simUpdater.startPeriodic(0.005);
+  }
+
+  /** Update {@link DCMotorSim} model and {@link TalonFX} {@link TalonFXSimState} */
+  private void updateSimulation() {
+    var simState = servo.getSimState();
+
+    // Calculate voltage /w friction from Talon sim state & apply to DCMotorSim model
+    double simVoltage = addFriction(simState.getMotorVoltage(), 0.25);
+    simVoltage *= invertVoltage ? -1 : 1;
+    sim.setInputVoltage(simVoltage);
+
+    // Update DCMotorSim
+    double timestamp = Timer.getFPGATimestamp();
+    sim.update(timestamp - previousUpdateTimestamp);
+    previousUpdateTimestamp = timestamp;
+
+    simAngle = sim.getAngularPosition();
+
+    // Set Talon sim state raw rotor pose and velocity from DCMotorSim model's state
+    simState.setRawRotorPosition(simAngle.div(config.Feedback.SensorToMechanismRatio));
+    simState.setRotorVelocity(sim.getAngularVelocity().div(config.Feedback.SensorToMechanismRatio));
   }
 
   /**
-   * Create new {@link ServoIOTalonFxSim} with a {@link FlywheelSim} backend
+   * @return angle of the {@link DCMotorSim} model
+   */
+  @Logged(name = "Physics Simulation Angle", importance = Importance.INFO)
+  public Angle getPhysicsSimulationAngle() {
+    return simAngle;
+  }
+
+  /**
+   * Enable inverted voltage
    *
-   * @param name friendly "nickname" for servo
-   * @param canDeviceId integer representing the CAN identification
-   * @param canBusId {@link CANBus} representing the CAN bus device is on
-   * @param config {@link TalonFXConfiguration} representing servo config
-   * @param positiveDirection {@link ChassisReference} representing the positive angular direction
-   *     of the servo
-   * @param flywheelSim {@link FlywheelSim} representing the physics model the simulation should use
-   *     as backend
+   * @param enabled boolean representing if inverted voltage should be enabled or disabled
    */
-  public ServoIOTalonFxSim(
-      String name,
-      int canDeviceId,
-      CANBus canBus,
-      TalonFXConfiguration config,
-      ChassisReference positiveDirection,
-      FlywheelSim flywheelSim) {
-    super(name, canDeviceId, canBus, config, Celsius.of(999));
-
-    this.positiveDirection = positiveDirection;
-
-    this.dcMotorSim = Optional.empty();
-    this.flywheelSim = Optional.of(flywheelSim);
-    this.armSim = Optional.empty();
-    this.elevatorSim = Optional.empty();
+  public void setInvertedVoltage(boolean enabled) {
+    invertVoltage = enabled;
   }
 
   /**
-   * Create new {@link ServoIOTalonFxSim} with a {@link SingleJointedArmSim} backend
-   *
-   * @param name friendly "nickname" for servo
-   * @param canDeviceId integer representing the CAN identification
-   * @param canBusId {@link CANBus} representing the CAN bus device is on
-   * @param config {@link TalonFXConfiguration} representing servo config
-   * @param positiveDirection {@link ChassisReference} representing the positive angular direction
-   *     of the servo
-   * @param armSim {@link SingleJointedArmSim} representing the physics model the simulation should
-   *     use as backend
+   * @param motorVoltage
+   * @param frictionVoltage
+   * @return
    */
-  public ServoIOTalonFxSim(
-      String name,
-      int canDeviceId,
-      CANBus canBus,
-      TalonFXConfiguration config,
-      ChassisReference positiveDirection,
-      SingleJointedArmSim armSim) {
-    super(name, canDeviceId, canBus, config, Celsius.of(999));
+  public static final double addFriction(double motorVoltage, double frictionVoltage) {
+    if (Math.abs(motorVoltage) < frictionVoltage) {
+      motorVoltage = 0.0;
+    } else if (motorVoltage > 0.0) {
+      motorVoltage -= frictionVoltage;
+    } else {
+      motorVoltage += frictionVoltage;
+    }
 
-    this.positiveDirection = positiveDirection;
-
-    this.dcMotorSim = Optional.empty();
-    this.flywheelSim = Optional.empty();
-    this.armSim = Optional.of(armSim);
-    this.elevatorSim = Optional.empty();
-  }
-
-  /**
-   * Create new {@link ServoIOTalonFxSim} with a {@link ElevatorSim} backend
-   *
-   * @param name friendly "nickname" for servo
-   * @param canDeviceId integer representing the CAN identification
-   * @param canBusId {@link CANBus} representing the CAN bus device is on
-   * @param config {@link TalonFXConfiguration} representing servo config
-   * @param positiveDirection {@link ChassisReference} representing the positive angular direction
-   *     of the servo
-   * @param elevatorSim {@link ElevatorSim} representing the physics model the simulation should use
-   *     as backend
-   */
-  public ServoIOTalonFxSim(
-      String name,
-      int canDeviceId,
-      CANBus canBus,
-      TalonFXConfiguration config,
-      ChassisReference positiveDirection,
-      ElevatorSim elevatorSim) {
-    super(name, canDeviceId, canBus, config, Celsius.of(999));
-
-    this.positiveDirection = positiveDirection;
-
-    this.dcMotorSim = Optional.empty();
-    this.flywheelSim = Optional.empty();
-    this.armSim = Optional.empty();
-    this.elevatorSim = Optional.of(elevatorSim);
-  }
-
-  @Override
-  public void periodic() {
-    super.periodic();
-
-    var simState = getTalonFXSimState();
-  }
-
-  /**
-   * @return {@link TalonFXSimState} for servo
-   */
-  // TODO finish writing simulation periodic logic
-  public TalonFXSimState getTalonFXSimState() {
-    var state = servo.getSimState();
-
-    state.Orientation = positiveDirection;
-
-    return state;
+    return motorVoltage;
   }
 }
