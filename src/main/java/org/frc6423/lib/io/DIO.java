8f065baad5c80ec226b3fda9d1c9432b2584d4d7
dabeycorn <Dasun20202020@gmail.com>
2026-01-17 23:10:24 -0500

    DIO: add configs for using debounce
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

--- src/main/java/org/frc6423/lib/io/DIO.java
+++ src/main/java/org/frc6423/lib/io/DIO.java
@@ -6,8 +6,13 @@
 
 package org.frc6423.lib.io;
 
+import static edu.wpi.first.units.Units.Seconds;
+
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.epilogue.Logged.Importance;
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.filter.Debouncer.DebounceType;
+import edu.wpi.first.units.measure.Time;
 
 /**
  * An {@link IO} for controlling a DIO device
@@ -15,6 +20,32 @@ import edu.wpi.first.epilogue.Logged.Importance;
  * @see https://docs.wpilib.org/en/stable/docs/hardware/sensors/digital-inputs-hardware.html
  */
 public abstract class DIO extends IO {
+  private final Debouncer debouncer;
+
+  /**
+   * Create new {@link DIO} /w a debouncer
+   *
+   * @param debounceTime {@link Time} representing how long signal must be true for state to be true
+   * @param debounceType {@link DebounceType} representing which transition the debouncer should be
+   *     applied to
+   */
+  public DIO(Time debounceTime, DebounceType debounceType) {
+    debouncer = new Debouncer(debounceTime.in(Seconds), debounceType);
+  }
+
+  @Override
+  protected void periodic() {
+    // Update debouncer periodically
+    getState();
+  }
+
+  /**
+   * @return true if state debouncer returns 1
+   */
+  public boolean getState() {
+    return debouncer.calculate(getRawState());
+  }
+
   /**
    * @return true if DIO port returns 1
    */
