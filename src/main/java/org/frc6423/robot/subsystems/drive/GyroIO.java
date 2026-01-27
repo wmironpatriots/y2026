53e02e947955b24e5d5f45a7f88c7e40da51062d
dabeycorn <Dasun20202020@gmail.com>
2026-01-27 15:19:25 -0500

    create blank drive classes
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

---
+++ src/main/java/org/frc6423/robot/subsystems/drive/GyroIO.java
@@ -0,0 +1,9 @@
+// Copyright (c) 2026 FRC 6423 - Ward Melville Iron Patriots
+// https://github.com/wmironpatriots
+// 
+// Open Source Software; you can modify and/or share it under the terms of
+// MIT license file in the root directory of this project
+
+package org.frc6423.robot.subsystems.drive;
+
+public class GyroIO {}
