caf64a5a06bad01e9628e1d6fe44d8cea7ed75ec
dabeycorn <Dasun20202020@gmail.com>
2026-01-26 22:42:31 -0500

    update match time constants to follow new code standards
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

--- src/main/java/org/frc6423/lib/game/Rebuilt.java
+++ src/main/java/org/frc6423/lib/game/Rebuilt.java
@@ -333,22 +333,22 @@ public class Rebuilt {
   // * MATCH TIME
 
   /** {@link Time} representing the total length of a match */
-  public static final Time MATCH_TIME = Seconds.of(2 * 60 + 20);
+  public static final Time kMatchLength = Seconds.of(2 * 60 + 20);
 
   /** {@link Time} representing the timestamp when shift zero ends */
-  public static final Time SHIFT_ZERO_END = Seconds.of(2 * 60 + 10);
+  public static final Time kEndOfShiftZero = Seconds.of(2 * 60 + 10);
 
   /** {@link Time} representing the timestamp when shift one ends */
-  public static final Time SHIFT_ONE_END = Seconds.of(60 + 45);
+  public static final Time kEndOfShiftOne = Seconds.of(60 + 45);
 
   /** {@link Time} representing the timestamp when shift two ends */
-  public static final Time SHIFT_TWO_END = Seconds.of(60 + 20);
+  public static final Time kEndOfShiftTwo = Seconds.of(60 + 20);
 
   /** {@link Time} representing the timestamp when shift three ends */
-  public static final Time SHIFT_THREE_END = Seconds.of(55);
+  public static final Time kEndOfShiftThree = Seconds.of(55);
 
   /** {@link Time} representing the timestamp when shift four ends */
-  public static final Time SHIFT_FOUR_END = Seconds.of(30);
+  public static final Time kEndOfShiftFour = Seconds.of(30);
 
   /**
    * @return true if robot alliance hub is active
@@ -362,10 +362,12 @@ public class Rebuilt {
       alliance = Optional.of(Alliance.Red);
     }
 
-    if (staringAlliance.isEmpty() || timestamp.gt(SHIFT_ZERO_END) || timestamp.lt(SHIFT_FOUR_END)) {
+    if (staringAlliance.isEmpty()
+        || timestamp.gt(kEndOfShiftZero)
+        || timestamp.lt(kEndOfShiftFour)) {
       return true;
-    } else if ((timestamp.lte(SHIFT_ZERO_END) && timestamp.gt(SHIFT_ONE_END))
-        || (timestamp.lte(SHIFT_TWO_END) && timestamp.gt(SHIFT_THREE_END))) {
+    } else if ((timestamp.lte(kEndOfShiftZero) && timestamp.gt(kEndOfShiftOne))
+        || (timestamp.lte(kEndOfShiftTwo) && timestamp.gt(kEndOfShiftThree))) {
       return staringAlliance.get() != alliance.get();
     } else {
       return staringAlliance.get() == alliance.get();
