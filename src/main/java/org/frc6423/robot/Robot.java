4386a012a79f7071a318387b85c5ad10eef1cbeb
dabeycorn <Dasun20202020@gmail.com>
2026-01-30 12:18:20 -0500

    revert Robot to original state
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

--- src/main/java/org/frc6423/robot/Robot.java
+++ src/main/java/org/frc6423/robot/Robot.java
@@ -6,15 +6,6 @@
 
 package org.frc6423.robot;
 
-import static edu.wpi.first.units.Units.Degrees;
-import static edu.wpi.first.units.Units.KilogramSquareMeters;
-import static edu.wpi.first.units.Units.Meters;
-import static edu.wpi.first.units.Units.Radians;
-import static edu.wpi.first.units.Units.Volts;
-
-import com.ctre.phoenix6.CANBus;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.configs.VoltageConfigs;
 import edu.wpi.first.epilogue.Epilogue;
 import edu.wpi.first.epilogue.EpilogueConfiguration;
 import edu.wpi.first.epilogue.Logged;
@@ -22,43 +13,17 @@ import edu.wpi.first.epilogue.Logged.Importance;
 import edu.wpi.first.epilogue.logging.LazyBackend;
 import edu.wpi.first.epilogue.logging.NTEpilogueBackend;
 import edu.wpi.first.epilogue.logging.errors.ErrorHandler;
-import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.Subsystem;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import org.frc6423.lib.driver.CommandRobot;
-import org.frc6423.lib.io.ServoIO;
-import org.frc6423.lib.io.ServoIO.Setpoint;
-import org.frc6423.lib.io.ServoIOTalonFxSim;
-import org.frc6423.lib.sim.PivotMechSim;
 
 @Logged
 public class Robot extends CommandRobot {
   private final CommandXboxController mDriverController, mOperatorController, mDevController;
-
-  private final ServoIO io =
-      new ServoIOTalonFxSim(
-          "bruh",
-          0,
-          new CANBus(),
-          new TalonFXConfiguration()
-              .withVoltage(
-                  new VoltageConfigs().withPeakForwardVoltage(16).withPeakReverseVoltage(16)),
-          new PivotMechSim(
-              new PivotMechSim.Config(
-                  DCMotor.getKrakenX60Foc(1),
-                  175,
-                  Meters.of(3),
-                  KilogramSquareMeters.of(0.001),
-                  false,
-                  Degrees.of(0.0),
-                  Degrees.of(90),
-                  Radians.zero())));
 
   public Robot() {
     // Initialize Devices
@@ -116,22 +81,8 @@ public class Robot extends CommandRobot {
     configureGameBehavior();
   }
 
-  private Subsystem subsystem = new SubsystemBase() {};
-
-  private Setpoint run = Setpoint.createVoltageSetpoint(Volts.of(12));
-  private Setpoint run2 = Setpoint.createVoltageSetpoint(Volts.of(-12));
-
   /** Define Driver & Operator controller bindings */
-  public void configureBindings() {
-    io.setFocStatus(true);
-    addPeriodic(() -> io.periodic(), 0.02);
-
-    mDriverController.button(1).whileTrue(subsystem.run(() -> io.applySetpoint(run)));
-
-    mDriverController.button(2).whileTrue(subsystem.run(() -> io.applySetpoint(run2)));
-
-    mDriverController.button(3).whileTrue(subsystem.run(() -> io.applySetpoint(Setpoint.stop())));
-  }
+  public void configureBindings() {}
 
   /** Define behavior during different oppmodes */
   public void configureGameBehavior() {}
