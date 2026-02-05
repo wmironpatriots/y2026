b8d57f2d71563b56a12888151ee6d7e1a3185847
dabeycorn <Dasun20202020@gmail.com>
2026-02-05 13:55:38 -0500

    git add module configs for cascade
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

--- src/main/java/org/frc6423/robot/subsystem/drive/constants/Cascade.java
+++ src/main/java/org/frc6423/robot/subsystem/drive/constants/Cascade.java
@@ -6,6 +6,7 @@
 
 package org.frc6423.robot.subsystem.drive.constants;
 
+import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.FeetPerSecond;
 import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
 import static edu.wpi.first.units.Units.RadiansPerSecondPerSecond;
@@ -13,6 +14,7 @@ import static edu.wpi.first.units.Units.RadiansPerSecondPerSecond;
 import edu.wpi.first.units.measure.AngularAcceleration;
 import edu.wpi.first.units.measure.LinearAcceleration;
 import edu.wpi.first.units.measure.LinearVelocity;
+import org.frc6423.robot.Constants.Matrix;
 
 /** {@link RebuiltL1} but for usage on cascade (just has modified gear ratios tbh) */
 public class Cascade extends RebuiltL1 {
@@ -37,5 +39,48 @@ public class Cascade extends RebuiltL1 {
   @Override
   public double getDriveRotorToMechRatio() {
     return (50.0 / 14.0) * (16.0 / 28.0) * (45.0 / 15.0);
+  }
+
+  @Override
+  public ModuleConfig[] getModuleConfigs() {
+    // TODO derive offsets
+    return new ModuleConfig[] {
+      new ModuleConfig(
+          "FR",
+          getCANBus(),
+          5,
+          6,
+          11,
+          getPivotServoConfig(Matrix.kDriveFrEncoderId),
+          getDriveServoConfig(),
+          getCANcoderConfig(Degrees.of(0.01))),
+      new ModuleConfig(
+          "FL",
+          getCANBus(),
+          3,
+          4,
+          10,
+          getPivotServoConfig(Matrix.kDriveFlEncoderId),
+          getDriveServoConfig(),
+          getCANcoderConfig(Degrees.of(-0.16))),
+      new ModuleConfig(
+          "BL",
+          getCANBus(),
+          1,
+          2,
+          9,
+          getPivotServoConfig(Matrix.kDriveBlEncoderId),
+          getDriveServoConfig(),
+          getCANcoderConfig(Degrees.of(0.36))),
+      new ModuleConfig(
+          "BR",
+          getCANBus(),
+          7,
+          8,
+          12,
+          getPivotServoConfig(Matrix.kDriveBrEncoderId),
+          getDriveServoConfig(),
+          getCANcoderConfig(Degrees.of(-0.26)))
+    };
   }
 }
