7217667da5cf4051336668bf78a9c0af769164e7
dabeycorn <Dasun20202020@gmail.com>
2026-02-05 11:30:21 -0500

    Create constants for Rebuilt robot in all 3 ratios
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

---
+++ src/main/java/org/frc6423/robot/subsystem/drive/constants/RebuiltL2.java
@@ -0,0 +1,39 @@
+// Copyright (c) 2026 FRC 6423 - Ward Melville Iron Patriots
+// https://github.com/wmironpatriots
+// 
+// Open Source Software; you can modify and/or share it under the terms of
+// MIT license file in the root directory of this project
+
+package org.frc6423.robot.subsystem.drive.constants;
+
+import static edu.wpi.first.units.Units.FeetPerSecond;
+import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
+import static edu.wpi.first.units.Units.RadiansPerSecondPerSecond;
+
+import edu.wpi.first.units.measure.AngularAcceleration;
+import edu.wpi.first.units.measure.LinearAcceleration;
+import edu.wpi.first.units.measure.LinearVelocity;
+
+/** {@link RebuiltL1} but for MK5i /w L2 ratio */
+public class RebuiltL2 extends RebuiltL1 {
+  @Override
+  public LinearVelocity getMaxLinearVelocity() {
+    // https://www.swervedrivespecialties.com/products/mk5i-swerve-module
+    return FeetPerSecond.of(17.4);
+  }
+
+  @Override
+  public LinearAcceleration getMaxLinearAcceleration() {
+    return MetersPerSecondPerSecond.of(12.624);
+  }
+
+  @Override
+  public AngularAcceleration getAngularAcceleration() {
+    return RadiansPerSecondPerSecond.of(50.022);
+  }
+
+  @Override
+  public double getDriveRotorToMechRatio() {
+    return (54.0 / 14.0) * (25.0 / 32.0) * (30.0 / 15.0);
+  }
+}
