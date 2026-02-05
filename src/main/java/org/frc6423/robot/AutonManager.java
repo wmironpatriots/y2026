0f0508fd8d5df6f7f531894b12f69f43611d79b0
Frank <frankwangli0326@gmail.com>
2026-02-04 19:44:15 -0500

    finish placeholder AutonManager class
    Signed-off-by: Frank <frankwangli0326@gmail.com>

--- src/main/java/org/frc6423/robot/AutonManager.java
+++ src/main/java/org/frc6423/robot/AutonManager.java
@@ -6,7 +6,8 @@
 
 package org.frc6423.robot;
 
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import choreo.auto.AutoChooser;
+import choreo.auto.AutoFactory;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -23,57 +24,46 @@ import edu.wpi.first.wpilibj2.command.Commands;
  * <p>Javadocs
  */
 public class AutonManager {
-  private final SendableChooser<Routines> mChooser = new SendableChooser<>();
+  private final AutoFactory mAutoFactory;
+  private final AutoChooser mAutoChooser;
 
   /** Create new {@link AutonManager} */
   public AutonManager() {
-    mChooser.setDefaultOption("Do Nothing", Routines.NOTHING);
-    mChooser.addOption("DepotShot", Routines.DEPOT_SHOT);
-    mChooser.addOption("NeutralShot", Routines.NEUTRAL_SHOT);
-    mChooser.addOption("SourceShot", Routines.SOURCE_SHOT);
+    mAutoChooser = new AutoChooser();
+    mAutoFactory = new AutoFactory(null, null, null, false, null);
 
-    SmartDashboard.putData(mChooser);
+    mAutoChooser.addCmd("Do Nothing", this::doNothing);
+    mAutoChooser.addCmd("DepotShot", this::depotShot);
+    mAutoChooser.addCmd("NeutralShot", this::neutralShot);
+    mAutoChooser.addCmd("SourceShot", this::sourceShot);
+
+    SmartDashboard.putData(mAutoChooser);
   }
 
   /**
    * @return {@link Command} representing the selected autonomous routine
    */
-  public Command getAutonomousCommand() {
-    if (mChooser.getSelected() == null) {
-      return Commands.none();
-    }
-
-    switch (mChooser.getSelected()) {
-      case DEPOT_SHOT:
-        return getDepotShotRoutineCommand();
-      case NEUTRAL_SHOT:
-        return getNeutralShotRoutineCommand();
-      case SOURCE_SHOT:
-        return getSourceShotRoutineCommand();
-      default:
-        return Commands.none();
-    }
+  public Command getSelectedCommand() {
+    return mAutoChooser.selectedCommandScheduler();
   }
 
-  /**
-   * @return {@link Command} representing Depot Shot (placeholder until drive integration)
-   */
-  public Command getDepotShotRoutineCommand() {
+  private Command doNothing() {
     return Commands.none();
   }
 
-  /**
-   * @return {@link Command} representing Neutral Shot (placeholder until drive integration)
-   */
-  public Command getNeutralShotRoutineCommand() {
-    return Commands.none();
+  private Command depotShot() {
+    return Commands.sequence(
+        mAutoFactory.resetOdometry("DepotShot"), mAutoFactory.trajectoryCmd("DepotShot"));
   }
 
-  /**
-   * @return {@link Command} representing Source Shot (placeholder until drive integration)
-   */
-  public Command getSourceShotRoutineCommand() {
-    return Commands.none();
+  private Command neutralShot() {
+    return Commands.sequence(
+        mAutoFactory.resetOdometry("NeutralShot"), mAutoFactory.trajectoryCmd("NeutralShot"));
+  }
+
+  private Command sourceShot() {
+    return Commands.sequence(
+        mAutoFactory.resetOdometry("SourceShot"), mAutoFactory.trajectoryCmd("SourceShot"));
   }
 
   /** Represents a possible routine during autonomous */
