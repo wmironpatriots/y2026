f8eaab2d89d9b047310073b4bbdae70629e47a6a
dabeycorn <Dasun20202020@gmail.com>
2026-02-06 13:16:48 -0500

    Create drive command backend method
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

--- src/main/java/org/frc6423/robot/subsystem/drive/Drive.java
+++ src/main/java/org/frc6423/robot/subsystem/drive/Drive.java
@@ -6,10 +6,19 @@
 
 package org.frc6423.robot.subsystem.drive;
 
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.epilogue.Logged.Importance;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -21,8 +30,10 @@ import org.frc6423.robot.subsystem.drive.constants.DriveConstants;
 import org.frc6423.robot.subsystem.drive.gyro.GyroIO;
 import org.frc6423.robot.subsystem.drive.gyro.GyroIOPigeon2;
 import org.frc6423.robot.subsystem.drive.module.SwerveModuleIO;
+import org.frc6423.robot.subsystem.drive.module.SwerveModuleIO.ControlMode;
 import org.frc6423.robot.subsystem.drive.module.SwerveModuleIOTalonFx;
 
+/** {@link SubsystemBase} extension representing swerve drivetrain */
 public class Drive extends SubsystemBase {
   private final DriveConstants mConstants;
 
@@ -30,8 +41,17 @@ public class Drive extends SubsystemBase {
   private final SwerveDriveKinematics mKinematics;
 
   private final SwerveModuleIO[] mModules;
-  private final GyroIO gyro;
+  private final GyroIO mGyro;
 
+  private SwerveModuleState[] mSetpointStates;
+
+  private Rotation2d mSimRotation2d = Rotation2d.kZero;
+
+  /**
+   * Create new {@link Drive}
+   *
+   * @param constants {@link DriveConstants} representing subsystem configuration
+   */
   public Drive(DriveConstants constants) {
     mConstants = constants;
 
@@ -40,7 +60,7 @@ public class Drive extends SubsystemBase {
 
     var configs = constants.getModuleConfigs();
     mModules = new SwerveModuleIO[configs.length];
-    gyro = new GyroIOPigeon2(mConstants.getGyroConfig());
+    mGyro = new GyroIOPigeon2(mConstants.getGyroConfig());
 
     for (int i = 0; i < configs.length; i++) {
       mModules[i] =
@@ -48,6 +68,8 @@ public class Drive extends SubsystemBase {
               ? new SwerveModuleIOTalonFx(configs[i].name(), configs[i], constants)
               : new SwerveModuleIOTalonFx(configs[i].name(), configs[i], constants);
     }
+
+    mSetpointStates = new SwerveModuleState[mModules.length];
   }
 
   @Override
@@ -71,20 +93,40 @@ public class Drive extends SubsystemBase {
   /**
    * @return {@link Rotation2d} representing measured yaw rotation
    */
+  @Logged(name = "Rotation2d", importance = Importance.INFO)
   public Rotation2d getRotation2d() {
-    return gyro.getRotation2d();
+    return Robot.isReal() ? mGyro.getRotation2d() : mSimRotation2d;
   }
 
   /**
    * @return {@link Rotation3d} representing the measured orientation of the robot in 3D space
    */
+  @Logged(name = "Rotation3d", importance = Importance.INFO)
   public Rotation3d getRotation3d() {
-    return gyro.getRotation3d();
+    return Robot.isReal() ? mGyro.getRotation3d() : new Rotation3d(mSimRotation2d);
+  }
+
+  /**
+   * @return {@link LinearVelocity} representing the linear velocity of drivetrain
+   */
+  @Logged(name = "LinearVelocity", importance = Importance.INFO)
+  public LinearVelocity getLinearVelocity() {
+    return MetersPerSecond.of(
+        Math.hypot(getChassisSpeeds().vxMetersPerSecond, getChassisSpeeds().vyMetersPerSecond));
+  }
+
+  /**
+   * @return {@link AngularVelocity} representing the angular velocity of drivetrain
+   */
+  @Logged(name = "AngularVelocity", importance = Importance.INFO)
+  public AngularVelocity getAngularVelocity() {
+    return RadiansPerSecond.of(getChassisSpeeds().omegaRadiansPerSecond);
   }
 
   /**
    * @return {@link SwerveModulePosition} representing the displacement vectors of swerve modules
    */
+  @Logged(name = "SwerveModulePositions", importance = Importance.INFO)
   public SwerveModulePosition[] getSwerveModulePositions() {
     var poses = new SwerveModulePosition[mModules.length];
     for (int i = 0; i < poses.length; i++) {
@@ -94,6 +136,72 @@ public class Drive extends SubsystemBase {
     return poses;
   }
 
-  // TODO
-  public void stop() {}
+  /**
+   * @return {@link SwerveModulePosition} representing the velocity vectors of swerve modules
+   */
+  @Logged(name = "SwerveModuleStates", importance = Importance.INFO)
+  public SwerveModuleState[] getSwerveModuleStates() {
+    var states = new SwerveModuleState[mModules.length];
+    for (int i = 0; i < states.length; i++) {
+      states[i] = mModules[i].getSwerveModuleState();
+    }
+
+    return states;
+  }
+
+  /**
+   * @return {@link SwerveModuleState} array representing the setpoint velocity vectors of swerve
+   *     modules
+   */
+  @Logged(name = "Rotation2d", importance = Importance.INFO)
+  public SwerveModuleState[] getSetpointSwerveModuleStates() {
+    return mSetpointStates;
+  }
+
+  /**
+   * @return {@link ChassisSpeeds} representing the drive speeds
+   */
+  @Logged(name = "ChassisSpeeds", importance = Importance.INFO)
+  public ChassisSpeeds getChassisSpeeds() {
+    return mKinematics.toChassisSpeeds(getSwerveModuleStates());
+  }
+
+  /**
+   * Set a velocity setpoint to optimized and run
+   *
+   * @param speeds {@link ChassisSpeeds} representing velocity setpoint
+   * @param openLoopEnabled when true, open-loop controlled will be utilized
+   */
+  public void setChassisSpeedsSetpoint(ChassisSpeeds speeds, boolean openLoopEnabled) {
+    // Generate a time specific setpoint from continuous-time speeds
+    speeds = ChassisSpeeds.discretize(speeds, 0.02);
+
+    // Convert to module states and clamp velocity
+    var states = mKinematics.toSwerveModuleStates(speeds);
+    SwerveDriveKinematics.desaturateWheelSpeeds(states, mConstants.getMaxLinearVelocity());
+
+    ControlMode mode;
+
+    // Use FOC control until over 90% of max velocity
+    if (getLinearVelocity().gt(mConstants.getMaxLinearVelocity().times(0.9))) {
+      mode = openLoopEnabled ? ControlMode.OPEN_LOOP_VOLT_FOC : ControlMode.CLOSED_LOOP_TORQUE_FOC;
+    } else {
+      mode = openLoopEnabled ? ControlMode.OPEN_LOOP_VOLT : ControlMode.CLOSED_LOOP_VOLT;
+    }
+
+    for (int i = 0; i < mModules.length; i++) {
+      mModules[i].setSetpoint(states[i], mode);
+    }
+
+    mSetpointStates = states;
+  }
+
+  /** Stop drivetrain completely */
+  public void stop() {
+    for (int i = 0; i < mModules.length; i++) {
+      mModules[i].stop();
+    }
+
+    mSetpointStates = new SwerveModuleState[mModules.length];
+  }
 }
