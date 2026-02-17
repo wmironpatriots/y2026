0f454aa5a21e5a42e81ecf0158184085dc40ae84
dabeycorn <Dasun20202020@gmail.com>
2026-02-17 13:39:21 -0500

    cleanup drive + superstucture
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

--- src/main/java/org/frc6423/robot/subsystem/Superstructure.java
+++ src/main/java/org/frc6423/robot/subsystem/Superstructure.java
@@ -6,70 +6,16 @@
 
 package org.frc6423.robot.subsystem;
 
-import edu.wpi.first.epilogue.Logged;
-import edu.wpi.first.epilogue.Logged.Importance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import org.frc6423.robot.RobotState;
-import org.frc6423.robot.subsystem.drive.Drive;
-import org.frc6423.robot.subsystem.flywheel.Flywheel;
-import org.frc6423.robot.subsystem.hood.Hood;
-import org.frc6423.robot.subsystem.indexer.Indexer;
-import org.frc6423.robot.subsystem.intake.Intake;
-import org.frc6423.robot.subsystem.led.Led;
 
 /** {@link SubsystemBase} extension for control all subsystems */
 public class Superstructure extends SubsystemBase {
   /** Represents a state of being robot can be in */
-  public static enum State {
-    PACKED,
-    UNPACKING,
-    IDLE
-  }
+  public static enum State {}
 
-  private final RobotState mRobotState = RobotState.getInstance();
-
-  private final Drive mDrive;
-
-  private final Intake mIntake;
-  private final Indexer mIndexer;
-  // TODO feeder
-  private final Flywheel mFlywheel;
-  private final Hood mHood;
-
-  private final Led mLed;
-
-  private State mState;
-  private State mPreviousState;
-
-  /**
-   * Create new {@link Superstructure}
-   *
-   * @param drive {@link Drive} representing drive subsystem
-   * @param intake {@link Intake} representing intake subsystem
-   * @param indexer {@link Indexer} representing indexer subsystem
-   * @param flywheel {@link Flywheel} representing flywheel subsystem
-   * @param hood {@link Hood} representing hood subsystem
-   * @param led {@link Led} representing LED subsystem
-   */
-  public Superstructure(
-      Drive drive, Intake intake, Indexer indexer, Flywheel flywheel, Hood hood, Led led) {
-    mDrive = drive;
-    mIntake = intake;
-    mIndexer = indexer;
-    // TODO feeder
-    mFlywheel = flywheel;
-    mHood = hood;
-    mLed = led;
-  }
+  /** Create new {@link Superstucture} */
+  public Superstructure() {}
 
   @Override
   public void periodic() {}
-
-  /**
-   * @return {@link State} representing current action robot is doing
-   */
-  @Logged(name = "State", importance = Importance.INFO)
-  public State getState() {
-    return mState;
-  }
 }
