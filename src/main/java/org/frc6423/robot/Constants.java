c73bd1584e259ab9f7d2dc5f79b23fff7328a4d3
dabeycorn <Dasun20202020@gmail.com>
2026-02-18 12:10:47 -0500

    correct can device ids in constants
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

--- src/main/java/org/frc6423/robot/Constants.java
+++ src/main/java/org/frc6423/robot/Constants.java
@@ -39,27 +39,36 @@ public final class Constants {
     public static final int kDriveBrPivotId = 1;
     public static final int kDriveBrEncoderId = 2;
     public static final int kDriveBrDriveId = 3;
+
     public static final int kDriveFrPivotId = 4;
     public static final int kDriveFrEncoderId = 5;
     public static final int kDriveFrDriveId = 6;
+
     public static final int kDriveFlPivotId = 7;
     public static final int kDriveFlEncoderId = 8;
     public static final int kDriveFlDriveId = 9;
+
     public static final int kDriveBlPivotId = 10;
     public static final int kDriveBlEncoderId = 11;
     public static final int kDriveBlDriveId = 12;
+
     public static final int kDriveGyroId = 13;
 
     public static final CANBus kSubsystemCanBus = new CANBus("SOUP");
 
-    public static final int kIntakePivotId = 1;
-    public static final int kIntakeEncoderId = 2;
-    public static final int kIntakeRollerId = 3;
-    public static final int kIndexerId = 4;
-    public static final int kFeederId = 5;
-    public static final int kHoodId = 6;
-    public static final int kFlywheelLeftId = 7;
-    public static final int kFlywheelRightId = 8;
+    public static final int kIntakePivotId = 14;
+    public static final int kIntakeEncoderId = 15;
+    public static final int kIntakeRollerId = 16;
+
+    public static final int kIndexerId = 17;
+
+    public static final int kFeederId = 18;
+
+    public static final int kHoodId = 19;
+    public static final int kHoodEncoderId = 20;
+
+    public static final int kFlywheelLeftId = 21;
+    public static final int kFlywheelRightId = 22;
   }
 
   /** Represents the type of robot codebase is running on */
