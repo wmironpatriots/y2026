6dc068e32ac4ca04ccac7560a805f62d063ddd73
dabeycorn <Dasun20202020@gmail.com>
2026-02-25 22:52:10 -0500

    final Flywheel changes for testing
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

--- src/main/java/org/frc6423/robot/subsystem/flywheel/Flywheel.java
+++ src/main/java/org/frc6423/robot/subsystem/flywheel/Flywheel.java
@@ -19,6 +19,8 @@ import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.Slot1Configs;
+import com.ctre.phoenix6.configs.Slot2Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
@@ -27,6 +29,7 @@ import edu.wpi.first.epilogue.Epilogue;
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.epilogue.Logged.Importance;
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.MomentOfInertia;
@@ -45,39 +48,48 @@ import org.frc6423.robot.Constants.Matrix;
 import org.frc6423.robot.Robot;
 
 /**
- * {@link SubsystemBase} extension representing the flywheel subsystem
+ * {@link SubsystemBase} Flywheel Subsystem
  *
- * <p>The {@link Flywheel} has two servos that drive it
+ * <p>Subsystem is powered by two Kraken x60s
  */
 public class Flywheel extends SubsystemBase {
-  /** {@Flywheel} subsystem constants */
+  /** Constants for the {@link Flywheel} */
   public class Constants {
     // * PHYSICAL CONSTANTS
-    /** {@link MomentOfInertia} representing the rotational inertia of flywheel system */
+    /** {@link MomentOfInertia} Rotational Inertia of flywheel system */
     public static final MomentOfInertia kRotationalInertia =
         KilogramSquareMeters.of(10.491008 * 0.0002926397);
 
     // * CONTROL CONSTANTS
-    /** {@link Double} representing the maximum allowed percent error in angular velocity */
+    /** {@link Double} Max allowed percent error in subsystem velocity */
     public static final double kEpsilon = 0.01;
 
     // * HARDWARE CONSTANTS
-    /** {@link Integer} representing the CAN ID of the left flywheel servo */
+    /** {@link Integer} CAN ID of the left flywheel servo */
     private static final int kLeftCanDeviceId = Matrix.kFlywheelLeftId;
 
-    /** {@link Integer} representing the CAN ID of the right flywheel servo */
+    /** {@link Integer} CAN ID of the right flywheel servo */
     private static final int kRightCanDeviceId = Matrix.kFlywheelLeftId;
 
-    /** {@link Current} representing the stator current limit of servos */
+    /** {@link Current} Stator current limit of servos */
     public static final Current kServoStatorCurrentLimit = Amps.of(120);
 
-    /** {@link Current} representing the supply current limit of servos */
+    /** {@link Current} Supply current limit of servos */
     public static final Current kServoSupplyCurrentLimit = Amps.of(40.0);
 
-    /** {@link Double} representing the gear ratio between the servo rotor and the flywheel shaft */
-    public static double kSensorToMechanismRatio = (28.0 / 16.0);
+    /** {@link Double} Gear ratio between the servo rotor and the flywheel shaft */
+    public static final double kSensorToMechanismRatio = (28.0 / 16.0);
 
-    /** {@link TalonFXConfiguration} representing the hardware config of the flywheel servos */
+    /** {@link Integer} Gains slot used for accelerating flywheel */
+    public static final int kGainSlotAccel = 0;
+
+    /** {@link Integer} Gains slot used for maintaing flywheel velocity */
+    public static final int kGainSlotMaintain = 0;
+
+    /** {@link Integer} Gains slot used for deaccelerating flywheel */
+    public static final int kGainSlotDeaccel = 0;
+
+    /** {@link TalonFXConfiguration} Hardware config of the flywheel servos */
     private static final TalonFXConfiguration kServoTalonConfig =
         new TalonFXConfiguration()
             .withAudio(new AudioConfigs().withBeepOnBoot(true).withBeepOnConfig(true))
@@ -98,9 +110,23 @@ public class Flywheel extends SubsystemBase {
                     .withKV(0.10759)
                     .withKA(3.6145)
                     .withKP(0.23165)
-                    .withKD(0.0));
+                    .withKD(0.0)) // Torque Current Control Gains (accelerating)
+            .withSlot1(
+                new Slot1Configs()
+                    .withKS(4.7248)
+                    .withKV(0.10759)
+                    .withKA(3.6145)
+                    .withKP(0.23165)
+                    .withKD(0.0)) // TODO Torque Current Control Gains (maintaing)
+            .withSlot2(
+                new Slot2Configs()
+                    .withKS(4.7248)
+                    .withKV(0.10759)
+                    .withKA(3.6145)
+                    .withKP(0.23165)
+                    .withKD(0.0)); // TODO Torque Current Control Gains (deaccelerating)
 
-    /** {@link CANBus} representing bus CAN devices are on */
+    /** {@link CANBus} CAN bus devices are on */
     public static final CANBus kCanBus = Matrix.kSubsystemCanBus;
   }
 
@@ -140,7 +166,7 @@ public class Flywheel extends SubsystemBase {
 
   private AngularVelocity mTargetVelocity = RevolutionsPerSecond.zero();
 
-  private final SysIdRoutine mCharacterization;
+  private final SysIdRoutine mSysIdRoutines;
 
   /**
    * Create new {@link Flywheel}
@@ -156,8 +182,8 @@ public class Flywheel extends SubsystemBase {
 
     mRight.setLeader(mLeft, true);
 
-    // Init Characterization Routines
-    mCharacterization =
+    // Init SysId
+    mSysIdRoutines =
         new SysIdRoutine(
             new SysIdRoutine.Config(
                 Volts.of(35).per(Second),
@@ -172,38 +198,67 @@ public class Flywheel extends SubsystemBase {
                 null,
                 this));
 
+    // Publish characterization command in tuning mode
     if (Flags.kTuningModeEnabled) {
-      SmartDashboard.putData("runCharacterization", characterize());
+      SmartDashboard.putData("Run SysId Characterization", runCharacterizationSequence());
     }
+
+    // Set default command
+    setDefaultCommand(coast());
   }
 
   @Override
   public void periodic() {
     mLeft.periodic();
     mRight.periodic();
+
+    // Switch between gain slots
+    if (mTargetVelocity.gt(getAngularVelocity())) {
+      mLeft.setGainsSlot(Constants.kGainSlotAccel);
+    } else if (isNearSetpoint()) {
+      mLeft.setGainsSlot(Constants.kGainSlotMaintain);
+    } else if (mTargetVelocity.lt(getAngularVelocity())) {
+      mLeft.setGainsSlot(Constants.kGainSlotDeaccel);
+    }
   }
 
   // * GETTERS
   /**
-   * @return {@link AngularVelocity} representing the angular velocity of flywheel
+   * Get Angular Position of subsystem
+   *
+   * @return {@link Angle}
    */
-  @Logged(name = "Angular Velocity", importance = Importance.INFO)
+  @Logged(name = "Angular Position (rads)", importance = Importance.INFO)
+  public Angle getAngle() {
+    return mLeft.getAngle();
+  }
+
+  /**
+   * Get Angular Velocity of subsystem
+   *
+   * @return {@link AngularVelocity}
+   */
+  @Logged(name = "Angular Velocity (rads per second)", importance = Importance.INFO)
   public AngularVelocity getAngularVelocity() {
     return mLeft.getAngularVelocity();
   }
 
   /**
-   * @return {@link AngularVelocity} representing the setpoint angular velocity of flywheel
+   * Get Setpoint Angular Velocity of subsystem
+   *
+   * @return {@link AngularVelocity}
    */
-  @Logged(name = "Setpoint Angular Velocity", importance = Importance.INFO)
+  @Logged(name = "Setpoint Angular Velocity (rads per second)", importance = Importance.INFO)
   public AngularVelocity getAngularVelocitySetpoint() {
     return mTargetVelocity;
   }
 
   /**
-   * @return true when the percent error from the setpoint angular velocity is less than the epsilon
+   * Check if subsystem is nearly at setpoint angular velocity
+   *
+   * @return {@link AngularVelocity}
    */
-  @Logged(name = "is Near Setpoint", importance = Importance.INFO)
+  @Logged(name = "is Near Setpoint (bool)", importance = Importance.INFO)
   public boolean isNearSetpoint() {
     return getAngularVelocity().in(RadiansPerSecond) / mTargetVelocity.in(RadiansPerSecond)
         > Constants.kEpsilon;
@@ -211,56 +266,74 @@ public class Flywheel extends SubsystemBase {
 
   // * COMMANDS
   /**
-   * Request subsystem to run System Identification routines for characterization
+   * Run SysId Characterization Routine for determining gains
+   *
+   * <p>Tests will run as follows: +Quasi, -Quasi, +Dyna, -Dyna
+   *
+   * <p>Tests will not start utnil flywheel has stopped running
    *
    * @return {@link Command}
    */
-  public Command characterize() {
+  public Command runCharacterizationSequence() {
     return Commands.sequence(
-        mCharacterization.quasistatic(Direction.kForward),
-        Commands.waitUntil(() -> isNearSetpoint()),
-        mCharacterization.quasistatic(Direction.kReverse),
-        Commands.waitUntil(() -> isNearSetpoint()),
-        mCharacterization.dynamic(Direction.kForward),
-        Commands.waitUntil(() -> isNearSetpoint()),
-        mCharacterization.dynamic(Direction.kReverse));
+            mSysIdRoutines.quasistatic(Direction.kForward),
+            Commands.waitUntil(() -> isNearSetpoint()),
+            mSysIdRoutines.quasistatic(Direction.kReverse),
+            Commands.waitUntil(() -> isNearSetpoint()),
+            mSysIdRoutines.dynamic(Direction.kForward),
+            Commands.waitUntil(() -> isNearSetpoint()),
+            mSysIdRoutines.dynamic(Direction.kReverse))
+        .beforeStarting(() -> mTargetVelocity = RevolutionsPerSecond.zero(), this)
+        .withName("Flywheel Characterization");
   }
 
   /**
-   * Request subsystem to stop applying output and coast
+   * Request subsystem to coast
+   *
+   * <p>When coast, flywheel servos will not apply any output; flywheel will be allowed to spin down
    *
    * @return {@link Command}
    */
   public Command coast() {
-    return this.run(
-        () -> {
-          mTargetVelocity = RevolutionsPerSecond.zero();
-          mLeft.stop();
-        });
+    return this.run(() -> mLeft.stop()).withName("FlywheelCoast");
   }
 
   /**
-   * Request subsystem to accelerate to a specified angular velocity
+   * TODO storage command
    *
-   * @param velocity {@link AngularVelocity} representing the velocity to accelerate to
+   * <p>This command should store surplus supply voltage
+   *
+   * <p>Make command public once finished
+   *
+   * @return {@link Command}
+   */
+  protected Command store() {
+    return Commands.none().withName("FlywheelStore");
+  }
+
+  /**
+   * Request subsystem to accelerate to desired angular velocity
+   *
+   * @param velocity {@link AngularVelocity} Angular Velocity setpoint for subsystem to accelerate
+   *     to
    * @return {@link Command}
    */
   public Command accelerateToVelocity(AngularVelocity velocity) {
-    return accelerateToVelocity(() -> velocity);
+    return accelerateToVelocity(() -> velocity).withName("FlywheelAccelerateTo");
   }
 
   /**
-   * Request subsystem to continiously accelerate to the angular velocity from a stream
+   * Request subsystem to continiously accelerate to a stream of angular velocity setpoints
    *
-   * @param velocity {@link Suppler} of {@link AngularVelocity} representing the velocity to
-   *     accelerate to
+   * @param velocity {@link Supplier}<{@link AngularVelocity}> Stream of angular velocity setpoints
    * @return {@link Command}
    */
   public Command accelerateToVelocity(Supplier<AngularVelocity> velocity) {
     return this.run(
-        () -> {
-          mTargetVelocity = velocity.get();
-          mLeft.setTorqueMotionProfiledVelocitySetpoint(mTargetVelocity);
-        });
+            () -> {
+              mTargetVelocity = velocity.get();
+              mLeft.setTorqueMotionProfiledVelocitySetpoint(mTargetVelocity);
+            })
+        .withName("FlywheelAccelerateToContinously");
   }
 }
