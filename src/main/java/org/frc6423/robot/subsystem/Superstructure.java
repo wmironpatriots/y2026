95c090871b126ab1b657abab28e87877a0a10a08
dabeycorn <Dasun20202020@gmail.com>
2026-02-28 17:19:46 -0500

    WIP state transitions
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

--- src/main/java/org/frc6423/robot/subsystem/Superstructure.java
+++ src/main/java/org/frc6423/robot/subsystem/Superstructure.java
@@ -6,91 +6,206 @@
 
 package org.frc6423.robot.subsystem;
 
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.epilogue.Logged.Importance;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.DoubleSupplier;
+import org.frc6423.robot.Rebuilt;
 import org.frc6423.robot.RobotState;
-import org.frc6423.robot.subsystem.feeder.Feeder;
-import org.frc6423.robot.subsystem.flywheel.Flywheel;
-import org.frc6423.robot.subsystem.hood.Hood;
-import org.frc6423.robot.subsystem.indexer.Indexer;
-import org.frc6423.robot.subsystem.intake.Intake;
 
-/**
- * TODO WIP {@link SubsystemBase} Manager Subsystem for controlling all other subsystem in sequence
- */
+/** WIP */
 public class Superstructure extends SubsystemBase {
-  private final RobotState mRobotState = RobotState.getInstance();
+  /** Constants for the {@link Superstructure} */
+  public class Constants {}
 
-  // * SUBSYSTEM MEMBERS
-  private final Intake mIntake = Intake.create();
-  private final Indexer mIndexer = Indexer.create();
-  private final Feeder mFeeder = Feeder.create();
-  private final Hood mHood = Hood.create();
-  private final Flywheel mFlywheel = Flywheel.create();
+  private final RobotState mRobotState = RobotState.getInstance();
 
   // * STATE MEMBERS
   private State mState = State.IDLE_NEUTRAL_ZONE;
-  private State mPreviouState = mState;
+  private Timer mTimer = new Timer();
 
-  // * DRIVER TRIGGERS
-  private final Trigger mIntakeTrigger;
-  private final Trigger mAlignTrigger;
-  private final Trigger mSpinupTrigger;
-  private final Trigger mFireTrigger;
+  // * TRIGGERS
+  @Logged
+  private final Trigger mInNeutralZone =
+      new Trigger(() -> Rebuilt.kNeutralZone.contains(mRobotState.getTranslation2d()));
 
-  private final DoubleSupplier mXVelSupplier;
-  private final DoubleSupplier mYVelSupplier;
-  private final DoubleSupplier mOmegaSupplier;
+  @Logged
+  private final Trigger mInTrenchZone =
+      new Trigger(() -> Rebuilt.kTrenchZone.contains(mRobotState.getTranslation2d()));
+
+  @Logged
+  private final Trigger mInAllianceZone =
+      new Trigger(() -> Rebuilt.kRobotAllianceZone.contains(mRobotState.getTranslation2d()));
+
+  // * DRIVER INPUT TRIGGERS
+  @Logged
+  private final Trigger mIntakeTrigger, mLeftAlign, mRightAlign, mSpinupTrigger, mFireTrigger;
+
+  @Logged private final DoubleSupplier mXVelocitySupplier;
+  @Logged private final DoubleSupplier mYVelocitySupplier;
+  @Logged private final DoubleSupplier mOmegaSupplier;
 
   // * STATE MAP
   private final Map<State, Trigger> mStateMap = new HashMap<>();
 
-  /**
-   * Create new {@link Superstructure}
-   *
-   * @param intakeTrigger {@link Trigger} Trigger for intaking
-   * @param actionTrigger {@link Trigger} Trigger for turning on intake align
-   * @param spinupTrigger {@link Trigger} Trigger for spinning up flywheel
-   * @param fireTrigger {@link Trigger} Trigger for firing fuel
-   * @param xVelSupplier {@link DoubleSupplier} Stream of requested drive x velocity magnitudes
-   * @param yVelSupplier {@link DoubleSupplier} Stream of requested drive y velocity magnitudes
-   * @param omegaSupplier {@link DoubleSupplier} Stream of request drive omega magnitudes
-   */
   public Superstructure(
       Trigger intakeTrigger,
-      Trigger alignTrigger,
+      Trigger leftAlignTrigger,
+      Trigger rightAlignTrigger,
       Trigger spinupTrigger,
       Trigger fireTrigger,
-      DoubleSupplier xVelSupplier,
-      DoubleSupplier yVelSupplier,
+      DoubleSupplier xVelocitySupplier,
+      DoubleSupplier yVelocitySupplier,
       DoubleSupplier omegaSupplier) {
+    // Define driver input triggers
     mIntakeTrigger = intakeTrigger;
-    mAlignTrigger = alignTrigger;
+    mLeftAlign = leftAlignTrigger;
+    mRightAlign = rightAlignTrigger;
     mSpinupTrigger = spinupTrigger;
     mFireTrigger = fireTrigger;
 
-    mXVelSupplier = xVelSupplier;
-    mYVelSupplier = yVelSupplier;
+    mXVelocitySupplier = xVelocitySupplier;
+    mYVelocitySupplier = yVelocitySupplier;
     mOmegaSupplier = omegaSupplier;
 
+    // Init state map
     for (var state : State.values()) {
       mStateMap.put(state, new Trigger(() -> mState == state));
     }
 
+    // Define state machine behavior
     defineStateBehavior();
+    defineStateTransitions();
+
+    // Start internal timer
+    mTimer.start();
   }
 
-  // TODO
+  /**
+   * Get active state
+   *
+   * @return {@link State}
+   */
+  @Logged(name = "State", importance = Importance.INFO)
+  public State getState() {
+    return mState;
+  }
+
   /** Define behavior during different states */
   private void defineStateBehavior() {}
 
-  // TODO
+  /** Define transitions between two states */
+  private void defineStateTransitions() {
+    // NEUTRAL ZONE TRANSITIONS
+    createTransition(State.IDLE_NEUTRAL_ZONE, State.IDLE_ALLIANCE_ZONE, mInAllianceZone);
+
+    createTransition(State.IDLE_NEUTRAL_ZONE, State.INTAKING, mIntakeTrigger);
+
+    createTransition(State.IDLE_NEUTRAL_ZONE, State.PREPPING_FERRY, mSpinupTrigger);
+
+    // FERRYING TRANSITIONS
+    createTransition(
+        State.PREPPING_FERRY,
+        State.FERRYING,
+        mFireTrigger.and(() -> false)); // TODO check for shooter ready
+
+    createTransition(State.PREPPING_FERRY, State.IDLE_NEUTRAL_ZONE, mSpinupTrigger.negate());
+
+    createTransition(
+        State.FERRYING,
+        State.PREPPING_FERRY,
+        new Trigger(() -> false)); // TODO check for shooter not ready
+
+    createTransition(
+        State.FERRYING, State.IDLE_NEUTRAL_ZONE, mFireTrigger.negate().and(mInNeutralZone));
+
+    createTransition(
+        State.FERRYING, State.IDLE_ALLIANCE_ZONE, mFireTrigger.negate().and(mInAllianceZone));
+
+    // ALLIANCE ZONE TRANSITIONS
+    createTransition(State.IDLE_ALLIANCE_ZONE, State.IDLE_NEUTRAL_ZONE, mInNeutralZone);
+
+    createTransition(State.IDLE_ALLIANCE_ZONE, State.INTAKING, mIntakeTrigger);
+
+    createTransition(State.IDLE_ALLIANCE_ZONE, State.PREPPING_SCORE, mSpinupTrigger);
+
+    // SCORING TRANSITIONS
+    createTransition(
+        State.PREPPING_SCORE,
+        State.SCORING,
+        mFireTrigger.and(() -> false)); // TODO check for shooter ready
+
+    createTransition(State.PREPPING_SCORE, State.IDLE_NEUTRAL_ZONE, mSpinupTrigger.negate());
+
+    createTransition(
+        State.SCORING,
+        State.PREPPING_FERRY,
+        new Trigger(() -> false)); // TODO check for shooter not ready
+
+    createTransition(
+        State.SCORING, State.IDLE_NEUTRAL_ZONE, mFireTrigger.negate().and(mInNeutralZone));
+
+    createTransition(
+        State.SCORING, State.IDLE_ALLIANCE_ZONE, mFireTrigger.negate().and(mInAllianceZone));
+
+    // INTAKING TRANSITIONS
+    createTransition(
+        State.INTAKING, State.IDLE_NEUTRAL_ZONE, mIntakeTrigger.negate().and(mInNeutralZone));
+
+    createTransition(
+        State.INTAKING, State.IDLE_ALLIANCE_ZONE, mIntakeTrigger.negate().and(mInAllianceZone));
+  }
+
+  /**
+   * Create a transition between two states
+   *
+   * @param startingState {@link State} Starting state
+   * @param endingState {@link State} Ending state
+   * @param requirement {@link Trigger} Requirement for the state transition to happen
+   */
+  private void createTransition(State startingState, State endingState, Trigger requirement) {
+    requirement.and(mStateMap.get(startingState)).onTrue(setState(endingState));
+  }
+
+  // * COMMANDS
+  /**
+   * Hard set current state to specified
+   *
+   * @param state {@link State} Desired state
+   * @return {@link Command}
+   */
+  public Command setState(State state) {
+    return Commands.runOnce(
+            () -> {
+              System.out.println("Transitioning " + mState.toString() + " to " + state.toString());
+              mTimer.reset();
+              mState = state;
+            })
+        .ignoringDisable(true)
+        .withName("State Change");
+  }
+
   /** A state of being a {@link Superstructure} can be in */
   public static enum State {
+    /** {@link State} Superstructure is idle in neutral zone */
     IDLE_NEUTRAL_ZONE,
-    IDLE_ALLIANC_ZONE,
+    /** {@link State} Superstructure is idle in alliance zone */
+    IDLE_ALLIANCE_ZONE,
+    /** {@link State} Superstucture is intaking */
+    INTAKING,
+    /** {@link State} Superstructure is spinning up for ferry */
+    PREPPING_FERRY,
+    /** {@link State} Superstructure is ferrying into alliance zone */
+    FERRYING,
+    /** {@link State} Superstructure is spinning up for scoring */
+    PREPPING_SCORE,
+    /** {@link State} Superstructure is firing into hub */
+    SCORING,
   }
 }
