459ce3d3de67e091105cb2f52259f5a76341f67a
dabeycorn <Dasun20202020@gmail.com>
2026-03-01 01:42:59 -0500

    move flywheel & hood classes to shooter package
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

--- src/main/java/org/frc6423/robot/subsystem/shooter/components/Hood.java
+++ src/main/java/org/frc6423/robot/subsystem/shooter/components/Hood.java
@@ -4,7 +4,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // MIT license file in the root directory of this project
 
-package org.frc6423.robot.subsystem.hood;
+package org.frc6423.robot.subsystem.shooter.components;
 
 import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Degrees;
