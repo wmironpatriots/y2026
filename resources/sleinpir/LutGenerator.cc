993a10e3a62b787d805b906c12a39f27d84ad219
dabeycorn <Dasun20202020@gmail.com>
2026-03-02 13:39:45 -0500

    add constants for the LutGenerator
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

--- resources/sleinpir/LutGenerator.cc
+++ resources/sleinpir/LutGenerator.cc
@@ -9,6 +9,7 @@
 #include <print>
 
 #include <Eigen/Core>
+#include <sleipnir/autodiff/variable_matrix.hpp>
 #include <sleipnir/optimization/problem.hpp>
 
 /*
@@ -22,6 +23,35 @@
     top constants: 
         - The maximum distance from the hub to calculate for (R)
         - The amount of steps within the max distance to calculate points for (dR)
+
+    All units are meters, mps, mpsps, & kg.
 */
 
-using Eigen::Vector3d;
+// ? PARAMETERS
+constexpr double R = 182.11 * 0.0254;
+constexpr double dR = R / 10;
+
+// * TARGET CHARACTERISTICS
+constexpr double kTargetEffectiveOpeningRadius = 47.0 / 2.0 * 0.0254;
+constexpr double kTargetEffectiveOpeningHeight = 72.0 * 0.0254;
+
+// * PROJECTILE CHARACTERISTICS
+/* The radius of fuel in meters */
+constexpr double kFuelRadius = 5.91 / 2 * 0.0254;
+/* The mass of fuel in meters */
+constexpr double kFuelMass = 10.0;
+
+/* 
+    The Density of a fuel 
+    V = (4.0 / 3.0) * pi * radius^2
+    rho = m / V
+*/
+// constexpr double kFuelRho = kFuelMass / ((4.0 / 3.0) * M_PI * kFuelRadius * kFuelRadius * kFuelRadius);
+constexpr double kFuelRho = 1.1; // TODO
+
+// * FORCES
+/** Vector of acceleration caused by gravity */
+constexpr auto kGravitationalAcceleration = Eigen::Vector3d{
+    {0.0}, 
+    {0.0}, 
+    {9.806}};
