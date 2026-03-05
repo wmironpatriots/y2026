5e2b4a7a0b5c02d4510a0cbd94626842e8f9eba5
dabeycorn <Dasun20202020@gmail.com>
2026-03-05 11:30:57 -0500

    more progress with the LUT
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

--- resources/sleinpir/LutGenerator.cc
+++ resources/sleinpir/LutGenerator.cc
@@ -9,7 +9,6 @@
 #include <print>
 
 #include <Eigen/Core>
-#include <sleipnir/autodiff/variable_matrix.hpp>
 #include <sleipnir/optimization/problem.hpp>
 
 /*
@@ -36,17 +35,10 @@ constexpr double kTargetEffectiveOpeningRadius = 47.0 / 2.0 * 0.0254;
 constexpr double kTargetEffectiveOpeningHeight = 72.0 * 0.0254;
 
 // * PROJECTILE CHARACTERISTICS
-/* The radius of fuel in meters */
-constexpr double kFuelRadius = 5.91 / 2 * 0.0254;
-/* The mass of fuel in meters */
+/* The mass of fuel in kilograms */
 constexpr double kFuelMass = 10.0;
 
-/* 
-    The Density of a fuel 
-    V = (4.0 / 3.0) * pi * radius^2
-    rho = m / V
-*/
-// constexpr double kFuelRho = kFuelMass / ((4.0 / 3.0) * M_PI * kFuelRadius * kFuelRadius * kFuelRadius);
+/* The Density of a fuel */
 constexpr double kFuelRho = 1.1; // TODO
 
 // * FORCES
@@ -55,3 +47,23 @@ constexpr auto kGravitationalAcceleration = Eigen::Vector3d{
     {0.0}, 
     {0.0}, 
     {9.806}};
+
+slp::VariableMatrix<double> function(const slp::VariableMatrix<double>& x) {
+    using namespace slp::slicing;
+
+    // r' = r'
+    // z' = z'
+    //
+    // [r", z"] = [0, -g] - (F_D(v)/m v_hat - F_L(v)/m (omega * v)
+
+    // rho is the fluid density in kg/(m^3)
+    // v is the linear velocity in meters/sec
+    // omega is the angular velocity in rads/sec
+    // A is the cross-sectional area of a circle in m^2
+    // m is the projectile mass in kg
+    auto v = x[slp::Slice{3, 6}, _];
+    slp::Variable v2 = v.T() * v;
+    auto v_norm = sqrt(v2);
+    auto v_hat = v / v_norm;
+    constexpr Eigen::Vector3d omega{{0.0}, {-2.0}, {0.0}};
+}
