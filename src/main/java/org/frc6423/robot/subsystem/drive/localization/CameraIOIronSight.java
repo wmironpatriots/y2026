4e1f8834398e6b276d7322710f6ba1c5a967de8f
dabeycorn <Dasun20202020@gmail.com>
2026-03-04 22:12:43 -0500

    create base cameraIO class
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

---
+++ src/main/java/org/frc6423/robot/subsystem/drive/localization/CameraIOIronSight.java
@@ -0,0 +1,56 @@
+// Copyright (c) 2026 FRC 6423 - Ward Melville Iron Patriots
+// https://github.com/wmironpatriots
+// 
+// Open Source Software; you can modify and/or share it under the terms of
+// MIT license file in the root directory of this project
+
+package org.frc6423.robot.subsystem.drive.localization;
+
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.networktables.DoubleSubscriber;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.PubSubOption;
+import edu.wpi.first.networktables.StructSubscriber;
+
+public class CameraIOIronSight extends CameraIO {
+  private final StructSubscriber<Pose2d> mEstimateSubscriber;
+  private final DoubleSubscriber mTimestampSubscriber;
+
+  /**
+   * Create new {@link CameraIOIronSight}
+   *
+   * @param name {@link String} An identifier for camera
+   */
+  public CameraIOIronSight(String name) {
+    super(name);
+
+    mEstimateSubscriber =
+        NetworkTableInstance.getDefault()
+            .getTable("iron-sight/estimates/" + name)
+            .getStructTopic("Pose2d", Pose2d.struct)
+            .subscribe(
+                new Pose2d(), PubSubOption.keepDuplicates(true), PubSubOption.periodic(0.01));
+
+    mTimestampSubscriber =
+        NetworkTableInstance.getDefault()
+            .getTable("iron-sight/estimates/" + name)
+            .getDoubleTopic("TimestampSeconds")
+            .subscribe(0.0, PubSubOption.keepDuplicates(true), PubSubOption.periodic(0.01));
+  }
+
+  @Override
+  public boolean isConnected() {
+    // TODO
+    return true;
+  }
+
+  @Override
+  public VisionMeasurement[] getUnreadMeasurements() {
+    // TODO get multiple when support is added
+    return new VisionMeasurement[] {
+      new VisionMeasurement(
+          mTimestampSubscriber.get(), mEstimateSubscriber.get(), VecBuilder.fill(0.0, 0.0, 0.0))
+    };
+  }
+}
