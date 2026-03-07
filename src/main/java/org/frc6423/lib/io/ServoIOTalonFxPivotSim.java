c254b17c048368a64499bf8d609666d2aed75d46
dabeycorn <Dasun20202020@gmail.com>
2026-03-07 02:00:01 -0500

    redo ServoIOTalonFxPivotSim
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

--- src/main/java/org/frc6423/lib/io/ServoIOTalonFxPivotSim.java
+++ src/main/java/org/frc6423/lib/io/ServoIOTalonFxPivotSim.java
@@ -6,10 +6,6 @@
 
 package org.frc6423.lib.io;
 
-import static edu.wpi.first.units.Units.KilogramSquareMeters;
-import static edu.wpi.first.units.Units.Meters;
-import static edu.wpi.first.units.Units.Radians;
-
 import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.configs.FeedbackConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
@@ -19,106 +15,88 @@ import com.ctre.phoenix6.sim.ChassisReference;
 import com.ctre.phoenix6.sim.TalonFXSimState.MotorType;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
-import edu.wpi.first.units.measure.Angle;
-import edu.wpi.first.units.measure.Distance;
-import edu.wpi.first.units.measure.MomentOfInertia;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 
-/** {@link ServoIOTalon} extension for pivot simulation */
 public class ServoIOTalonFxPivotSim extends ServoIOTalonFx {
-  private final SingleJointedArmSim mPhysicsModel;
+  private final SingleJointedArmSim mModel;
 
   private final double mRatio;
 
-  private double previousTimestamp;
+  private double mPreviousTimestamp;
   private final Notifier mNotifier;
 
-  /**
-   * Create new {@link ServoIOTalonFxRollerSim}
-   *
-   * @param name {@link String} representing servo nickname
-   * @param canBus {@link CANBus} representing CAN bus loop device is in
-   * @param canDeviceId {@link Integer} representing the id of CAN device
-   * @param talonConfig {@link TalonFXConfiguration} representing the servo config
-   * @param rotationalInertia {@link MomentOfInertia} representing the rotational inertia of system
-   * @param motorType {@link MotorType} representing the type of motors used for gearbox input
-   * @param gearbox {@link DCMotor} representing the gearbox input
-   * @param rotorToMechanismRatio {@link Double} representing the gear ratio between the servo rotor
-   *     and the mechanism
-   */
-  public ServoIOTalonFxPivotSim(
+  protected ServoIOTalonFxPivotSim(
       String name,
+      MotorType type,
       CANBus canBus,
-      int deviceId,
-      TalonFXConfiguration talonConfig,
-      MomentOfInertia rotationalInertia,
-      Distance armLength,
-      Angle minAngle,
-      Angle maxAngle,
-      Angle startingAngle,
-      boolean simulateGravity,
-      MotorType motorType,
+      int canDeviceId,
+      TalonFXConfiguration config,
+      double rotationalInertiaKgSquaredMeters,
+      double armLengthMeters,
+      double minAngleDegrees,
+      double maxAngleDegrees,
+      double startingAngleDegrees,
+      boolean shouldSimulateGravity,
       DCMotor gearbox,
-      double rotorToMechanismRatio) {
-    super(name, canBus, deviceId, talonConfig);
+      double rotorToMechRatio) {
+    super(name, type, canBus, canDeviceId, config);
 
-    mPhysicsModel =
+    mRatio = rotorToMechRatio;
+
+    // Define Physics Model
+    mModel =
         new SingleJointedArmSim(
             LinearSystemId.createSingleJointedArmSystem(
-                gearbox, rotationalInertia.in(KilogramSquareMeters), rotorToMechanismRatio),
+                gearbox, rotationalInertiaKgSquaredMeters, rotorToMechRatio),
             gearbox,
-            rotorToMechanismRatio,
-            armLength.in(Meters),
-            minAngle.in(Radians),
-            maxAngle.in(Radians),
-            simulateGravity,
-            startingAngle.in(Radians));
+            rotorToMechRatio,
+            armLengthMeters,
+            Units.degreesToRadians(minAngleDegrees),
+            Units.degreesToRadians(maxAngleDegrees),
+            shouldSimulateGravity,
+            Units.degreesToRadians(startingAngleDegrees));
 
+    // Configure Phoneix 6 Sim
     mTalonConfig =
         mTalonConfig.withFeedback(
-            new FeedbackConfigs().withFeedbackSensorSource(FeedbackSensorSourceValue.RotorSensor));
-
-    mRatio = rotorToMechanismRatio;
+            new FeedbackConfigs()
+                .withFeedbackSensorSource(FeedbackSensorSourceValue.RotorSensor)
+                .withRotorToSensorRatio(1.0)
+                .withSensorToMechanismRatio(mRatio));
 
     mServo.getConfigurator().apply(mTalonConfig);
 
-    mServo.getSimState().setMotorType(motorType);
+    mServo.getSimState().setMotorType(type);
     mServo.getSimState().Orientation =
         mTalonConfig.MotorOutput.Inverted == InvertedValue.CounterClockwise_Positive
             ? ChassisReference.CounterClockwise_Positive
             : ChassisReference.Clockwise_Positive;
 
+    // Start loop
     mNotifier =
         new Notifier(
             () -> {
               final double timestamp = Timer.getFPGATimestamp();
-              final double deltaTime = timestamp - previousTimestamp;
-              previousTimestamp = timestamp;
+              final double deltaTime = timestamp - mPreviousTimestamp;
+              mPreviousTimestamp = timestamp;
 
               mServo.getSimState().setSupplyVoltage(RobotController.getBatteryVoltage());
 
-              mPhysicsModel.setInputVoltage(mServo.getSimState().getMotorVoltage());
-              mPhysicsModel.update(deltaTime);
+              mModel.setInputVoltage(mServo.getSimState().getMotorVoltage());
+              mModel.update(deltaTime);
 
               mServo
                   .getSimState()
-                  .setRawRotorPosition(
-                      (mPhysicsModel.getAngleRads() / (Math.PI * 2)) * rotorToMechanismRatio);
+                  .setRawRotorPosition((mModel.getAngleRads() / (Math.PI * 2)) * mRatio);
               mServo
                   .getSimState()
-                  .setRotorVelocity(
-                      (mPhysicsModel.getVelocityRadPerSec() / (Math.PI * 2))
-                          * rotorToMechanismRatio);
+                  .setRotorVelocity((mModel.getVelocityRadPerSec() / (Math.PI * 2)) * mRatio);
             });
 
     mNotifier.startPeriodic(0.002);
-  }
-
-  @Override
-  public Angle getAngle() {
-    return mRawPositionSignal.getValue().div(mRatio);
   }
 }
